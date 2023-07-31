02a7f832757c3d77d625c5dd3d0fb835ed8ebb9f
pushedrumex <pushedrumex@naver.com>
2023-07-31 15:36:56 +0900

    chore: 테스트용 상품 데이터를 세팅한다

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO product(name, image_url, price) VALUES('치킨', 'https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/product/3904681644/B.jpg?452000000', 18000);
+INSERT INTO product(name, image_url, price) VALUES('치킨', 'https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/product/3904681644/B.jpg?452000000', 18000);
+INSERT INTO product(name, image_url, price) VALUES('치킨', 'https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/product/3904681644/B.jpg?452000000', 18000);
+INSERT INTO product(name, image_url, price) VALUES('치킨', 'https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/product/3904681644/B.jpg?452000000', 18000);
+INSERT INTO product(name, image_url, price) VALUES('치킨', 'https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/product/3904681644/B.jpg?452000000', 18000);
+INSERT INTO product(name, image_url, price) VALUES('치킨', 'https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/product/3904681644/B.jpg?452000000', 18000);
+INSERT INTO product(name, image_url, price) VALUES('치킨', 'https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/product/3904681644/B.jpg?452000000', 18000);
+INSERT INTO product(name, image_url, price) VALUES('치킨', 'https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/product/3904681644/B.jpg?452000000', 18000);
