e6e3992ab5be7ab06fd1ccffc18fa28455d4556c
gunner6603 <gunner6603@snu.ac.kr>
2023-07-31 19:36:04 +0900

    feat: UserEntity 추가

---
+++ src/main/java/shopping/domain/entity/UserEntity.java
@@ -0,0 +1,39 @@
+package shopping.domain.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class UserEntity {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String email;
+    private String password;
+
+    public UserEntity() {
+    }
+
+    public UserEntity(final Long id, final String email, final String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public UserEntity(final String email, final String password) {
+        this(null, email, password);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
