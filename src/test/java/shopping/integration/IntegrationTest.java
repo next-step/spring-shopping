08df92c87beb48f69e8f63b6ca4eaf4b8c0010be
gunner6603 <gunner6603@snu.ac.kr>
2023-07-31 15:49:35 +0900

    test: 통합 테스트 작성

--- src/test/java/shopping/integration/IntegrationTest.java
+++ src/test/java/shopping/integration/IntegrationTest.java
@@ -2,11 +2,9 @@ package shopping.integration;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import shopping.repository.CartItemRepository;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class IntegrationTest {
@@ -14,12 +12,8 @@ public class IntegrationTest {
     @LocalServerPort
     int port;
 
-    @Autowired
-    CartItemRepository cartItemRepository;
-
     @BeforeEach
     public void setUp() {
         RestAssured.port = port;
-        cartItemRepository.deleteAll();
     }
 }
