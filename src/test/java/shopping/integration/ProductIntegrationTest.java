f9c46a3923417a8c49117facd0bc4de09450ab02
gunner6603 <gunner6603@snu.ac.kr>
2023-07-31 15:49:35 +0900

    test: 통합 테스트 작성

---
+++ src/test/java/shopping/integration/ProductIntegrationTest.java
@@ -0,0 +1,11 @@
+package shopping.integration;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class ProductIntegrationTest extends IntegrationTest {
+
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+    }
+}
