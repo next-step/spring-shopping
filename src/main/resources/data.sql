690b024ef443bda7bf0db26ae899cad4d47ed914
gunner6603 <gunner6603@snu.ac.kr>
2023-07-31 20:52:17 +0900

    fix: UserEntity table 이름 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,4 @@ insert into product(id, name, image_uuid_file_name, price) values(1, '치킨', '
 insert into product(id, name, image_uuid_file_name, price) values(2, '피자', 'pizza.png', 25000);
 insert into product(id, name, image_uuid_file_name, price) values(3, '사케', 'sake.png', 30000);
 
-insert into user(id, email, password) values(1, 'test_email@woowafriends.com', 'test_password!');
+insert into users(id, email, password) values(1, 'test_email@woowafriends.com', 'test_password!');
