035b76cb06378aecf85f2d10aa92f334a42ace9f
bkRyusim <bkryusim@gmail.com>
2023-07-31 13:53:31 +0900

    feat: Product 엔티티 구현

--- src/main/java/shopping/domain/Product.java
+++ src/main/java/shopping/domain/Product.java
@@ -1,7 +1,34 @@
 package shopping.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column
+    private int price;
+
+    protected Product() {
+    }
+
     public Product(Long id, String name, String image, int price) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.price = price;
     }
 }
