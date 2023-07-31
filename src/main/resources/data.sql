b19d7c5200eafe153c0acd7a82e1dd160df913a9
whatasame <whatasame@gmail.com>
2023-07-31 16:33:10 +0900

    docs: 상품 샘플 데이터 추가
    Co-authored-by: worldii <bbc2788@naver.com>

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into product(image_url, name, price)
+values ('/assets/img/chicken.png', '치킨', 20000),
+       ('/assets/img/hamburger.jpeg', '햄버거', 10000),
+       ('/assets/img/pizza.jpg', '피자', 20000);
