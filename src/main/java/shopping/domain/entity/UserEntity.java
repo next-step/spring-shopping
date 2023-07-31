5578cd094b5ed8e4dbcbfdfd12250e1ab84f3522
gunner6603 <gunner6603@snu.ac.kr>
2023-07-31 20:52:17 +0900

    fix: UserEntity table 이름 수정

--- src/main/java/shopping/domain/entity/UserEntity.java
+++ src/main/java/shopping/domain/entity/UserEntity.java
@@ -1,16 +1,18 @@
 package shopping.domain.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class UserEntity {
 
     @Id @GeneratedValue
     private Long id;
+    @Column(unique = true)
     private String email;
     private String password;
 
