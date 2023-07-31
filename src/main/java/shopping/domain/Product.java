5470b127e2b10f022bb44f0e5fc989bfccd9caf4
pushedrumex <pushedrumex@naver.com>
2023-07-31 17:07:28 +0900

    test: Product 생성자는 Product를 생성한다.

--- src/main/java/shopping/domain/Product.java
+++ src/main/java/shopping/domain/Product.java
@@ -6,23 +6,37 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Objects;
 
 @Entity
 @Table(name = "product")
 public class Product {
 
+    public static final int MAX_NAME_LENGTH = 25;
+    public static final int MIN_PRICE = 0;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 25, nullable = false)
+    @Column(length = MAX_NAME_LENGTH, nullable = false, unique = true)
     private String name;
 
     @Column(nullable = false)
     private String imageUrl;
 
     @Column(nullable = false)
-    private Long price;
+    private long price;
+
+    public Product(String name, String imageUrl, Long price) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.price = price;
+    }
+
+    protected Product() {
+
+    }
 
     public Long getId() {
         return id;
@@ -38,5 +52,18 @@ public class Product {
 
     public Long getPrice() {
         return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
