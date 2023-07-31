eb41f572b1cac4f90c603675c9aa78e5c9497bbd
gunner6603 <gunner6603@snu.ac.kr>
2023-07-31 17:01:15 +0900

    feat: data.sql 작성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into product(id, name, image_uuid_file_name, price) values(1, '치킨', 'fried_chicken.png', 20000);
+insert into product(id, name, image_uuid_file_name, price) values(2, '피자', 'pizza.png', 25000);
+insert into product(id, name, image_uuid_file_name, price) values(3, '사케', 'sake.png', 30000);
