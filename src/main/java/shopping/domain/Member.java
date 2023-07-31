4a16219cedb4daf0ca922e4cd102706bde41c985
pushedrumex <pushedrumex@naver.com>
2023-07-31 19:12:48 +0900

    feat: Member 생성자는 Member를 생성한다

--- src/main/java/shopping/domain/Member.java
+++ src/main/java/shopping/domain/Member.java
@@ -1,15 +1,20 @@
 package shopping.domain;
 
 import org.springframework.util.StringUtils;
+import shopping.exception.MemberException;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.regex.Pattern;
 
 @Entity
 public class Member {
+
+    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN);
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,12 +35,17 @@ public class Member {
 
     private void validatePassword(String password) {
         if (StringUtils.isEmpty(password)) {
-
+            throw new MemberException("사용자 이름이 존재하지 않습니다");
         }
     }
 
     private void validateEmail(String email) {
-
+        if (StringUtils.isEmpty(email)) {
+            throw new MemberException("사용자 이메일이 존재하지 않습니다");
+        }
+        if (!pattern.matcher(email).matches()) {
+            throw new MemberException("이메일 형식이 올바르지 않습니다");
+        }
     }
 
     protected Member() {
