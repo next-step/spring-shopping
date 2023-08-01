3757355438b0e943e6f13777ff3140c26745d203
devxb <dlwnsdud205@naver.com>
2023-08-01 16:37:56 +0900

    test: Cart 인수테스트를 작성한다

--- src/test/java/shopping/accept/AssertHelper.java
+++ src/test/java/shopping/accept/AssertHelper.java
@@ -6,9 +6,12 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import java.util.Arrays;
+import java.util.Optional;
 import org.hamcrest.Matchers;
 import org.springframework.http.HttpStatus;
 import shopping.advice.ErrorTemplate;
+import shopping.dto.CartResponse;
+import shopping.dto.CartResponse.ProductResponse;
 import shopping.dto.TokenResponse;
 
 class AssertHelper {
@@ -35,6 +38,79 @@ class AssertHelper {
 
             assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
             assertThat(errorTemplate.getStatusCode()).isEqualTo("AUTH-401");
+        }
+
+        static void assertTokenDeprecateFailed(final ExtractableResponse<Response> response) {
+            ErrorTemplate errorTemplate = response.as(ErrorTemplate.class);
+
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+            assertThat(errorTemplate.getStatusCode()).isEqualTo("AUTH-INTERCEPTOR-401");
+        }
+    }
+
+    static final class Cart {
+
+        static void assertProductNotFound(final ExtractableResponse<Response> result) {
+            assertThat(result.as(ErrorTemplate.class).getStatusCode()).isEqualTo("CART-SERVICE-401");
+        }
+
+        static void assertCart(final ExtractableResponse<Response> result, CartResponse exactlyExpected) {
+            Http.assertIsOk(result);
+
+            assertThat(result.as(CartResponse.class).getProductResponses()).isEqualTo(
+                    exactlyExpected.getProductResponses());
+        }
+
+        static void assertCartUpdated(ExtractableResponse<Response> findResult, long id, int expectedCount) {
+            Http.assertIsOk(findResult);
+            CartResponse cartResponse = findResult.as(CartResponse.class);
+            Optional<ProductResponse> optionalProductResponse = cartResponse.getProductResponses()
+                    .stream()
+                    .filter(productResponse -> productResponse.getId() == id)
+                    .findFirst();
+
+            assertThat(optionalProductResponse).isPresent();
+            assertThat(optionalProductResponse.get().getCount()).isEqualTo(expectedCount);
+        }
+
+        static void assertDeleteFailed(ExtractableResponse<Response> result) {
+            Http.assertIsBadRequest(result);
+
+            ErrorTemplate errorTemplate = result.as(ErrorTemplate.class);
+            assertThat(errorTemplate.getStatusCode()).isEqualTo("CART-401");
+        }
+
+        static void assertUpdatableProductNotFound(ExtractableResponse<Response> result) {
+            Http.assertIsBadRequest(result);
+
+            ErrorTemplate errorTemplate = result.as(ErrorTemplate.class);
+            assertThat(errorTemplate.getStatusCode()).isEqualTo("CART-402");
+        }
+
+        static void assertUpdateCountNotPositive(ExtractableResponse<Response> result) {
+            Http.assertIsBadRequest(result);
+
+            ErrorTemplate errorTemplate = result.as(ErrorTemplate.class);
+            assertThat(errorTemplate.getStatusCode()).isEqualTo("CART-403");
+        }
+    }
+
+    static final class Http {
+
+        static void assertIsOk(final ExtractableResponse<Response> response) {
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        }
+
+        static void assertIsCreated(final ExtractableResponse<Response> response) {
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        }
+
+        static void assertIsNoContent(final ExtractableResponse<Response> response) {
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+        }
+
+        public static void assertIsBadRequest(ExtractableResponse<Response> response) {
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
         }
     }
 }
--- src/test/java/shopping/accept/UrlHelper.java
+++ src/test/java/shopping/accept/UrlHelper.java
@@ -6,6 +6,8 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import org.springframework.http.MediaType;
+import shopping.dto.CartAddRequest;
+import shopping.dto.CartUpdateRequest;
 import shopping.dto.LoginRequest;
 
 class UrlHelper {
@@ -28,6 +30,51 @@ class UrlHelper {
                     .contentType(MediaType.APPLICATION_JSON_VALUE)
                     .accept(MediaType.APPLICATION_JSON_VALUE)
                     .when().post("/login/token")
+                    .then().log().all()
+                    .extract();
+        }
+    }
+
+    static final class Cart {
+
+        static ExtractableResponse<Response> addProduct(CartAddRequest cartAddRequest, String accessToken) {
+            return given().log().all()
+                    .auth().oauth2(accessToken)
+                    .body(cartAddRequest)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .accept(MediaType.APPLICATION_JSON_VALUE)
+                    .when().post("/carts")
+                    .then().log().all()
+                    .extract();
+        }
+
+        static ExtractableResponse<Response> findCart(String accessToken) {
+            return given().log().all()
+                    .auth().oauth2(accessToken)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .accept(MediaType.APPLICATION_JSON_VALUE)
+                    .when().get("/carts")
+                    .then().log().all()
+                    .extract();
+        }
+
+        static ExtractableResponse<Response> updateCart(CartUpdateRequest cartUpdateRequest, String accessToken) {
+            return given().log().all()
+                    .auth().oauth2(accessToken)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .accept(MediaType.APPLICATION_JSON_VALUE)
+                    .body(cartUpdateRequest)
+                    .when().patch("/carts")
+                    .then().log().all()
+                    .extract();
+        }
+
+        public static ExtractableResponse<Response> deleteCart(long deleteTargetId, String accessToken) {
+            return given().log().all()
+                    .auth().oauth2(accessToken)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .param("product-id", deleteTargetId)
+                    .when().delete("/carts")
                     .then().log().all()
                     .extract();
         }
