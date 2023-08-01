c18b21d8c8407a468504f2d8d5378cec4f9b710b
bkRyusim <bkryusim@gmail.com>
2023-08-01 20:08:28 +0900

    feat: LoginRequest 유효성 검사 테스트 및 구현

--- src/main/java/shopping/dto/LoginRequest.java
+++ src/main/java/shopping/dto/LoginRequest.java
@@ -1,12 +1,31 @@
 package shopping.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import shopping.exception.ShoppingException;
+
 public class LoginRequest {
     private final String email;
     private final String password;
 
+    @JsonCreator
     public LoginRequest(String email, String password) {
+        validateEmailNotNull(email);
+        validatePasswordNotNull(password);
+
         this.email = email;
         this.password = password;
+    }
+
+    private void validatePasswordNotNull(String password) {
+        if (password == null) {
+            throw new ShoppingException("password는 필수 항목입니다.");
+        }
+    }
+
+    private void validateEmailNotNull(String email) {
+        if (email == null) {
+            throw new ShoppingException("email은 필수 항목입니다.");
+        }
     }
 
     public String getEmail() {
