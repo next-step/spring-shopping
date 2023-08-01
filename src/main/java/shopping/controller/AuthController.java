8735a720b4e812467bc8ad9b9c22cf8e3b5d5ea4
devxb <dlwnsdud205@naver.com>
2023-08-01 14:48:49 +0900

    step2: 로그인 기능을 완성한다

--- src/main/java/shopping/controller/AuthController.java
+++ src/main/java/shopping/controller/AuthController.java
@@ -3,7 +3,6 @@ package shopping.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import shopping.dto.LoginRequest;
@@ -11,7 +10,6 @@ import shopping.dto.TokenResponse;
 import shopping.service.AuthService;
 
 @RestController
-@RequestMapping("/login")
 public class AuthController {
 
     private final AuthService authService;
@@ -20,7 +18,7 @@ public class AuthController {
         this.authService = authService;
     }
 
-    @PostMapping("/token")
+    @PostMapping("/login/token")
     @ResponseStatus(HttpStatus.OK)
     public TokenResponse login(@RequestBody LoginRequest request) {
         return authService.authenticate(request);
