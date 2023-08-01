a1d6b83c7bfb97ff81923b2a1f399e83c81ad209
bkRyusim <bkryusim@gmail.com>
2023-08-01 16:28:31 +0900

    feat: 장바구니 항목 수량 변경 Controller, Service 구현

--- src/main/java/shopping/application/CartService.java
+++ src/main/java/shopping/application/CartService.java
@@ -5,6 +5,7 @@ import org.springframework.transaction.annotation.Transactional;
 import shopping.domain.CartItem;
 import shopping.domain.Product;
 import shopping.domain.User;
+import shopping.dto.CartItemQuantityRequest;
 import shopping.dto.CartRequest;
 import shopping.dto.CartResponse;
 import shopping.repository.CartItemRespository;
@@ -40,9 +41,19 @@ public class CartService {
 
     @Transactional(readOnly = true)
     public List<CartResponse> findAll(Long userId) {
-        return cartItemRespository.findAll()
+        return userRepository.findById(userId)
+                .orElseThrow()
+                .getCartItems()
                 .stream()
                 .map(CartResponse::from)
                 .collect(Collectors.toUnmodifiableList());
+    }
+
+    @Transactional
+    public void update(CartItemQuantityRequest request, Long cartItemId, Long userId) {
+        User user = userRepository.findById(userId).orElseThrow();
+        CartItem item = cartItemRespository.findById(cartItemId).orElseThrow();
+
+        item.updateQuantity(request.getQuantity());
     }
 }
--- src/main/java/shopping/ui/CartController.java
+++ src/main/java/shopping/ui/CartController.java
@@ -3,12 +3,15 @@ package shopping.ui;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import shopping.application.CartService;
+import shopping.dto.CartItemQuantityRequest;
 import shopping.dto.CartRequest;
 import shopping.dto.CartResponse;
 import shopping.infrastructure.JwtProvider;
@@ -39,5 +42,14 @@ public class CartController {
     public ResponseEntity<List<CartResponse>> findAll(@RequestHeader(value = "Authorization") String bearerToken) {
         final String token = bearerToken.split(" ")[1];
         return ResponseEntity.ok().body(cartService.findAll(Long.parseLong(jwtProvider.getPayload(token))));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Void> updateQuantity(@RequestBody CartItemQuantityRequest request,
+                                               @PathVariable Long id,
+                                               @RequestHeader(value = "Authorization") String bearerToken) {
+        final String token = bearerToken.split(" ")[1];
+        cartService.update(request, id, Long.parseLong(jwtProvider.getPayload(token)));
+        return ResponseEntity.noContent().build();
     }
 }
