b553b6cc42d7a8c8202b2d7a8626841678ef6308
chanwooDev <pove2019@gmail.com>
2023-08-01 17:19:47 +0900

    feat: 로그인 컨트롤러 구현, 테스트 구현
    - 로그인이 정상적으로 성공한다
    - 로그인 유저 정보가 존재하지 않은 경우 예외를 던진다.
    - 로그인 비밀번호가 틀린 경우 예외를 던진다.
    - 비밀번호가 일치하지 않으면 false를 반환한다.

--- src/test/resources/test_data.sql
+++ src/test/resources/test_data.sql
@@ -4,3 +4,4 @@ INSERT INTO PRODUCT(file_name, image_store_type, name, amount) VALUES ('https://
 INSERT INTO PRODUCT(file_name, image_store_type, name, amount) VALUES ('https://cdn.imweb.me/upload/S20220826948cbdc34dca3/c0963c069da43.png','NONE', '치킨', 35000.00);
 INSERT INTO PRODUCT(file_name, image_store_type, name, amount) VALUES ('https://cdn.imweb.me/upload/S20220826948cbdc34dca3/c0963c069da43.png','NONE', '햄버거', 5000.00);
 
+insert into member(email, password) values ('hello@woowa.com', 'hello1');
