5c977c1f4f75413de3557e59b450603719778d29
devxb <dlwnsdud205@naver.com>
2023-08-01 13:41:35 +0900

    feat: User email, password 검증 로직을 추가한다

--- src/main/java/shopping/domain/User.java
+++ src/main/java/shopping/domain/User.java
@@ -5,15 +5,15 @@ import java.util.Objects;
 import shopping.domain.exception.StatusCodeException;
 import shopping.domain.status.UserExceptionStatus;
 
-// TODO: EMAIL, PASSWORD 최소, 최대길이 검증 로직  추가
 public final class User {
 
+    public static final int MAXIMUM_EMAIL_LENGTH = 100;
     private static final String EMAIL_REGEX =
             "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
     private static final String SPECIAL_CHARACTER_REGEX =
             ".*[!@#$%^&*(),.?\":{}|<>].*";
     private static final int MINIMUM_PASSWORD_LENGTH = 8;
-
+    private static final int MAXIMUM_PASSWORD_LENGTH = 100;
     private final Long id;
     private final String email;
     private final String password;
@@ -31,20 +31,30 @@ public final class User {
     }
 
     private void validateEmail(final String email) {
-        if (email.matches(EMAIL_REGEX)) {
+        if (isValidEmail(email)) {
             return;
         }
         throw new StatusCodeException(MessageFormat.format("\"{0}\" 은 사용할 수 없는 이메일입니다.", email),
                 UserExceptionStatus.INVALID_EMAIL.getStatus());
     }
 
+    private boolean isValidEmail(String email) {
+        return email.length() <= MAXIMUM_EMAIL_LENGTH && email.matches(EMAIL_REGEX);
+    }
+
     private void validatePassword(final String password) {
-        if (password.length() >= MINIMUM_PASSWORD_LENGTH && password.matches(SPECIAL_CHARACTER_REGEX)) {
+        if (isValidPassword(password)) {
             return;
         }
         throw new StatusCodeException(MessageFormat.format("비밀번호 \"{0}\" 는 특수문자를 포함하면서 {1} 자 이상이어야 합니다.",
                 password, MINIMUM_PASSWORD_LENGTH), UserExceptionStatus.INVALID_PASSWORD.getStatus());
     }
+
+    private boolean isValidPassword(String password) {
+        return MINIMUM_PASSWORD_LENGTH <= password.length() && password.length() <= MAXIMUM_PASSWORD_LENGTH
+                && password.matches(SPECIAL_CHARACTER_REGEX);
+    }
+
 
     public Long getId() {
         return id;
