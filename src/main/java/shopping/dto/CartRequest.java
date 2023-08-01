2951efafabf2be8f49ce0fbb8211b191184ce618
bkRyusim <bkryusim@gmail.com>
2023-08-01 19:54:37 +0900

    feat: CartRequest 유효성 검사 테스트 및 구현

--- src/main/java/shopping/dto/CartRequest.java
+++ src/main/java/shopping/dto/CartRequest.java
@@ -1,13 +1,32 @@
 package shopping.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import shopping.exception.ShoppingException;
+
 public class CartRequest {
     private Long productId;
 
     private CartRequest() {
     }
 
+    @JsonCreator
     public CartRequest(Long productId) {
+        validateNotNull(productId);
+        validatePositive(productId);
+        
         this.productId = productId;
+    }
+
+    private void validatePositive(Long productId) {
+        if (productId <= 0) {
+            throw new ShoppingException("productId는 양의 정수입니다.");
+        }
+    }
+
+    private void validateNotNull(Long productId) {
+        if (productId == null) {
+            throw new ShoppingException("productId는 필수 항목입니다.");
+        }
     }
 
     public Long getProductId() {
