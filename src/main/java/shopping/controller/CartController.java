7fcf25f10bfba8c941d1f097d557c64a40269cd7
gunner6603 <gunner6603@snu.ac.kr>
2023-08-01 19:51:14 +0900

    feat: 장바구니 조회 기능 구현

--- src/main/java/shopping/controller/CartController.java
+++ src/main/java/shopping/controller/CartController.java
@@ -1,15 +1,18 @@
 package shopping.controller;
 
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import shopping.argumentresolver.annotation.UserId;
 import shopping.dto.request.CartItemAddRequest;
+import shopping.dto.response.CartItemResponse;
 import shopping.service.CartService;
 
 @Controller
@@ -23,7 +26,7 @@ public class CartController {
         this.cartService = cartService;
     }
 
-    @PostMapping
+    @PostMapping("/items")
     @ResponseBody
     public ResponseEntity<Void> addCartItem(@RequestBody CartItemAddRequest cartItemAddRequest,
         @UserId Long userId) {
@@ -31,4 +34,17 @@ public class CartController {
         cartService.addCartItem(cartItemAddRequest, userId);
         return ResponseEntity.ok().build();
     }
+
+    @GetMapping("/items")
+    @ResponseBody
+    public ResponseEntity<List<CartItemResponse>> getCartItems(@UserId Long userId) {
+        List<CartItemResponse> cartItems = cartService.getCartItems(userId);
+        return ResponseEntity.ok(cartItems);
+    }
+
+    @GetMapping
+    public String cartPage() {
+        return "cart";
+    }
+
 }
--- src/test/java/shopping/integration/CartIntegrationTest.java
+++ src/test/java/shopping/integration/CartIntegrationTest.java
@@ -1,22 +1,24 @@
 package shopping.integration;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import org.assertj.core.api.Assertions;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import shopping.TestFixture;
 import shopping.dto.request.CartItemAddRequest;
 import shopping.dto.request.LoginRequest;
+import shopping.dto.response.CartItemResponse;
 import shopping.dto.response.LoginResponse;
 
 @DisplayName("CartIntegrationTest")
-class CartIntegrationTest extends IntegrationTest{
+class CartIntegrationTest extends IntegrationTest {
 
     @Override
     @BeforeEach
@@ -30,13 +32,8 @@ class CartIntegrationTest extends IntegrationTest{
         /* given */
         final LoginRequest loginRequest = new LoginRequest("test_email@woowafriends.com",
             "test_password!");
-        String accessToken = RestAssured
-            .given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(loginRequest)
-            .when().post("/user/login/token")
-            .then().log().all()
-            .extract().as(LoginResponse.class).getAccessToken();
+        String accessToken = TestFixture.login(loginRequest).as(LoginResponse.class)
+            .getAccessToken();
 
         final CartItemAddRequest cartItemAddRequest = new CartItemAddRequest(1L);
 
@@ -46,11 +43,38 @@ class CartIntegrationTest extends IntegrationTest{
             .auth().oauth2(accessToken)
             .contentType(MediaType.APPLICATION_JSON_VALUE)
             .body(cartItemAddRequest)
-            .when().post("/cart")
+            .when().post("/cart/items")
             .then().log().all()
             .extract();
 
         /* then */
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
+
+    @Test
+    @DisplayName("성공 : 장바구니에 있는 아이템 목록을 조회한다")
+    void successReadItem() {
+        /* given */
+        final LoginRequest loginRequest = new LoginRequest("test_email@woowafriends.com",
+            "test_password!");
+        String accessToken = TestFixture.login(loginRequest).as(LoginResponse.class)
+            .getAccessToken();
+
+        final CartItemAddRequest cartChicken = new CartItemAddRequest(1L);
+        final CartItemAddRequest cartPizza = new CartItemAddRequest(2L);
+        TestFixture.addCartItem(accessToken, cartChicken);
+        TestFixture.addCartItem(accessToken, cartPizza);
+
+        /* when */
+        final ExtractableResponse<Response> response = RestAssured.given().log().all()
+            .auth().oauth2(accessToken)
+            .when().get("/cart/items")
+            .then().log().all().extract();
+
+        /* then */
+        List<CartItemResponse> cartItems = response.jsonPath().getList(".", CartItemResponse.class);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(cartItems).hasSize(2);
+    }
+
 }
