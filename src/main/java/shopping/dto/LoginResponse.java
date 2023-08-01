63e93464410697751a7fcc77ba2f5e887d02ce79
pushedrumex <pushedrumex@naver.com>
2023-08-01 15:02:22 +0900

    feat: 로그인 dto 생성

---
+++ src/main/java/shopping/dto/LoginResponse.java
@@ -0,0 +1,14 @@
+package shopping.dto;
+
+public class LoginResponse {
+
+    private final String accessToken;
+
+    public LoginResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+}
