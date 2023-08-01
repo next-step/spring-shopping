0f81933ec9b3f7b9dea4b9babb83cb98945cf975
pushedrumex <pushedrumex@naver.com>
2023-08-01 14:47:26 +0900

    chore: Member 스키마를 정의하고, 테스트용 데이터를 생성한다

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
+-- product table
 INSERT INTO product(name, image_url, price) VALUES('치킨', 'https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/product/3904681644/B.jpg?452000000', 18000);
 INSERT INTO product(name, image_url, price) VALUES('피자', 'https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/product/3904681644/B.jpg?452000000', 18000);
 INSERT INTO product(name, image_url, price) VALUES('마라샹궈', 'https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/product/3904681644/B.jpg?452000000', 18000);
@@ -6,3 +7,7 @@ INSERT INTO product(name, image_url, price) VALUES('만두', 'https://cdn.011st.
 INSERT INTO product(name, image_url, price) VALUES('제육볶음', 'https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/product/3904681644/B.jpg?452000000', 18000);
 INSERT INTO product(name, image_url, price) VALUES('서브웨이', 'https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/product/3904681644/B.jpg?452000000', 18000);
 INSERT INTO product(name, image_url, price) VALUES('돈까스', 'https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/product/3904681644/B.jpg?452000000', 18000);
+
+-- member table
+INSERT INTO member(email, password) VALUES('woowa1@woowa.com', '1234');
+INSERT INTO member(email, password) VALUES('woowa2@woowa.com', '1234');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,14 @@
-CREATE TABLE product (
-     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-     name VARCHAR(25) NOT NULL UNIQUE,
-     image_url VARCHAR(255) NOT NULL,
-     price BIGINT NOT NULL
+CREATE TABLE product
+(
+    id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name      VARCHAR(25)  NOT NULL UNIQUE,
+    image_url VARCHAR(255) NOT NULL,
+    price     BIGINT       NOT NULL
+);
+
+CREATE TABLE member
+(
+    id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email      VARCHAR(255)  NOT NULL UNIQUE,
+    password    VARCHAR(255) NOT NULL
 );
