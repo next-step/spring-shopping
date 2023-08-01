39a75bdf6f2311d87bb1332c7607e9568958e057
gunner6603 <gunner6603@snu.ac.kr>
2023-08-01 14:39:46 +0900

    feat: 장바구니 아이템 추가 기능 구현

--- src/main/java/shopping/service/CartService.java
+++ src/main/java/shopping/service/CartService.java
@@ -1,18 +1,11 @@
 package shopping.service;
 
-import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import shopping.domain.CartItem;
 import shopping.domain.entity.CartItemEntity;
 import shopping.domain.entity.ProductEntity;
 import shopping.domain.entity.UserEntity;
 import shopping.dto.request.CartItemAddRequest;
-import shopping.dto.request.CartItemUpdateRequest;
-import shopping.dto.response.CartItemResponse;
-import shopping.exception.ErrorCode;
-import shopping.exception.ShoppingException;
 import shopping.repository.CartItemRepository;
 import shopping.repository.ProductRepository;
 import shopping.repository.UserRepository;
@@ -35,59 +28,11 @@ public class CartService {
 
     @Transactional
     public void addCartItem(CartItemAddRequest cartItemAddRequest, Long userId) {
-        final Long productId = cartItemAddRequest.getProductId();
-        final ProductEntity product = findProductBy(productId);
-        validateDuplicateCartItem(userId, productId);
-
         final UserEntity userProxy = userRepository.getReferenceById(userId);
+        final ProductEntity product = productRepository.findById(cartItemAddRequest.getProductId())
+            .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 상품 ID입니다."));
         final CartItemEntity cartItemEntity = new CartItemEntity(userProxy, product);
         cartItemRepository.save(cartItemEntity);
-    }
-
-    public List<CartItemResponse> getCartItems(final Long userId) {
-        return cartItemRepository.findByUserId(userId).stream()
-            .map(CartItemResponse::from)
-            .collect(Collectors.toUnmodifiableList());
-    }
-
-    @Transactional
-    public void updateCartItemQuantity(final Long cartItemId,
-        final CartItemUpdateRequest cartItemUpdateRequest, final Long userId) {
-        final int updateQuantity = cartItemUpdateRequest.getQuantity();
-        final CartItemEntity cartItemEntity = findCartItemEntityBy(cartItemId);
-
-        CartItem cartItem = CartItem.of(cartItemEntity, updateQuantity);
-        cartItem.matchUser(userId);
-        if (cartItem.isQuantityZero()) {
-            cartItemRepository.delete(cartItemEntity);
-            return;
-        }
-
-        cartItemEntity.updateQuantity(updateQuantity);
-    }
-
-    @Transactional
-    public void removeCartItem(final Long cartItemId, final Long userId) {
-        final CartItemEntity cartItemEntity = findCartItemEntityBy(cartItemId);
-        final CartItem cartItem = CartItem.from(cartItemEntity);
-        cartItem.matchUser(userId);
-        cartItemRepository.delete(cartItemEntity);
-    }
-
-    private CartItemEntity findCartItemEntityBy(final Long cartItemId) {
-        return cartItemRepository.findById(cartItemId)
-            .orElseThrow(() -> new ShoppingException(ErrorCode.INVALID_CART_ITEM));
-    }
-
-    private ProductEntity findProductBy(final Long productId) {
-        return productRepository.findById(productId)
-            .orElseThrow(() -> new ShoppingException(ErrorCode.INVALID_PRODUCT));
-    }
-
-    private void validateDuplicateCartItem(final Long userId, final Long productId) {
-        if (cartItemRepository.existsByUserIdAndProductId(userId, productId)) {
-            throw new ShoppingException(ErrorCode.DUPLICATE_CART_ITEM);
-        }
     }
 
 }
--- src/test/java/shopping/service/CartServiceTest.java
+++ src/test/java/shopping/service/CartServiceTest.java
@@ -1,11 +1,9 @@
 package shopping.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,12 +11,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import shopping.domain.entity.CartItemEntity;
 import shopping.domain.entity.ProductEntity;
 import shopping.domain.entity.UserEntity;
 import shopping.dto.request.CartItemAddRequest;
-import shopping.dto.request.CartItemUpdateRequest;
-import shopping.dto.response.CartItemResponse;
 import shopping.repository.CartItemRepository;
 import shopping.repository.ProductRepository;
 import shopping.repository.UserRepository;
@@ -38,7 +33,7 @@ class CartServiceTest {
 
     @Test
     @DisplayName("성공 : 장바구니에 상품을 추가한다.")
-    void successAddCartItem() {
+    void successAddItem() {
         /* given */
         Long productId = 1L;
         Long userId = 1L;
@@ -55,68 +50,5 @@ class CartServiceTest {
         verify(userRepository).getReferenceById(userId);
         verify(productRepository).findById(productId);
         verify(cartItemRepository).save(any());
-    }
-
-    @Test
-    @DisplayName("성공 : 장바구니 상품을 조회한다.")
-    void successReadCartItems() {
-        /* given */
-        Long userId = 1L;
-        UserEntity user = new UserEntity(userId, "test_email@woowafriends.com", "test_password!");
-        ProductEntity chicken = new ProductEntity(1L, "치킨", "fried_chicken.png", 20000);
-        ProductEntity pizza = new ProductEntity(2L, "피자", "pizza.png", 25000);
-        CartItemEntity cartItemChicken = new CartItemEntity(1L, user, chicken, 1);
-        CartItemEntity cartItemPizza = new CartItemEntity(2L, user, pizza, 1);
-        List<CartItemEntity> cartItems = List.of(cartItemChicken, cartItemPizza);
-        when(cartItemRepository.findByUserId(userId)).thenReturn(cartItems);
-
-        /* when */
-        List<CartItemResponse> cartItemResponses = cartService.getCartItems(userId);
-
-        /* then */
-        verify(cartItemRepository).findByUserId(userId);
-        assertThat(cartItemResponses).hasSize(cartItems.size());
-    }
-
-    @Test
-    @DisplayName("성공 : 장바구니 상품 갯수를 수정한다.")
-    void successUpdateCartItem() {
-        /* given */
-        Long userId = 1L;
-        UserEntity user = new UserEntity(userId, "test_email@woowafriends.com", "test_password!");
-        ProductEntity chicken = new ProductEntity(1L, "치킨", "fried_chicken.png", 20000);
-        CartItemEntity cartItemChicken = new CartItemEntity(1L, user, chicken, 1);
-
-        Long cartItemId = cartItemChicken.getId();
-        int updateQuantity = 3;
-        when(cartItemRepository.findById(cartItemId)).thenReturn(Optional.of(cartItemChicken));
-
-        /* when */
-        cartService.updateCartItemQuantity(cartItemId, new CartItemUpdateRequest(updateQuantity),
-            userId);
-
-        /* then */
-        assertThat(cartItemChicken.getQuantity()).isEqualTo(updateQuantity);
-
-    }
-
-    @Test
-    @DisplayName("성공 : 장바구니 상품을 삭제한다.")
-    void successDeleteCartItem() {
-        /* given */
-        Long userId = 1L;
-        UserEntity user = new UserEntity(userId, "test_email@woowafriends.com", "test_password!");
-        ProductEntity chicken = new ProductEntity(1L, "치킨", "fried_chicken.png", 20000);
-        CartItemEntity cartItemChicken = new CartItemEntity(1L, user, chicken, 1);
-
-        Long cartItemId = cartItemChicken.getId();
-        when(cartItemRepository.findById(cartItemId)).thenReturn(Optional.of(cartItemChicken));
-
-        /* when */
-        cartService.removeCartItem(cartItemId, userId);
-
-        /* then */
-        verify(cartItemRepository).findById(cartItemId);
-        verify(cartItemRepository).delete(cartItemChicken);
     }
 }
