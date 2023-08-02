cd3712073fdf16228a884d32f68baefb9ea21127
bkRyusim <bkryusim@gmail.com>
2023-08-02 15:03:57 +0900

    test: 이메일 형싱 유효성 검사 도메인 단위 테스트

--- src/test/java/shopping/domain/EmailTest.java
+++ src/test/java/shopping/domain/EmailTest.java
@@ -2,6 +2,8 @@ package shopping.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import shopping.exception.ShoppingException;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
@@ -23,5 +25,22 @@ class EmailTest {
         assertThatCode(() -> new Email("a".repeat(51)))
                 .isInstanceOf(ShoppingException.class)
                 .hasMessage("이메일 길이는 50자를 넘을 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("이메일은 올바른 형식을 따른다.")
+    void createEmailWithValidForm() {
+        // when, then
+        assertThatNoException().isThrownBy(() -> new Email("seungyoon@woowa.com"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abcd", "a@b@c", "abcd@abc.a", "ab*cd@gmail.com"})
+    @DisplayName("올바르지 않은 형식의 이메일을 생성하면 오류를 반환한다.")
+    void createEmailWithInvalidForm(String email) {
+        // when, then
+        assertThatCode(() -> new Email(email))
+                .isInstanceOf(ShoppingException.class)
+                .hasMessage("이메일 형식이 올바르지 않습니다.");
     }
 }
