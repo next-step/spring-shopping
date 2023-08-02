aef757a2b2c1f49a9424412d9dd77a11a07b5a64
bkRyusim <bkryusim@gmail.com>
2023-08-02 14:49:54 +0900

    feat: 이메일 길이 유효성 검증 로직 구현

--- src/main/java/shopping/domain/Email.java
+++ src/main/java/shopping/domain/Email.java
@@ -1,9 +1,13 @@
 package shopping.domain;
 
+import shopping.exception.ShoppingException;
+
 import javax.persistence.Embeddable;
 
 @Embeddable
 public class Email {
+
+    private static final int MAX_LENGTH = 50;
 
     private final String value;
 
@@ -12,7 +16,15 @@ public class Email {
     }
 
     public Email(String value) {
+        validateLength(value);
+
         this.value = value;
+    }
+
+    private void validateLength(String value) {
+        if (value.length() > MAX_LENGTH) {
+            throw new ShoppingException("이메일 길이는 50자를 넘을 수 없습니다.");
+        }
     }
 
     public String getValue() {
