980cedd3f92320068f33cbc018e3c22e6df8a72c
bkRyusim <bkryusim@gmail.com>
2023-08-02 14:31:00 +0900

    feat: 이름 길이 유효성 로직 구현

--- src/main/java/shopping/domain/Name.java
+++ src/main/java/shopping/domain/Name.java
@@ -1,10 +1,14 @@
 package shopping.domain;
 
+import shopping.exception.ShoppingException;
+
 import javax.persistence.Embeddable;
 
 @Embeddable
 public class Name {
 
+    private static final int MAX_LENGTH = 20;
+    
     private final String value;
 
     protected Name() {
@@ -12,7 +16,15 @@ public class Name {
     }
 
     public Name(String value) {
+        validateLength(value);
+
         this.value = value;
+    }
+
+    private void validateLength(String value) {
+        if (value.length() > MAX_LENGTH) {
+            throw new ShoppingException("이름은 20자를 넘을 수 없습니다.");
+        }
     }
 
     public String getValue() {
