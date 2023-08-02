32dcce3cf0d331e42175952a182fa51f41a8e285
chanwooDev <pove2019@gmail.com>
2023-08-02 15:23:49 +0900

    feat: RestAssuredUtils추가, 오류페이지 응답코드 수정

--- src/main/java/shopping/exception/CommonExceptionHandler.java
+++ src/main/java/shopping/exception/CommonExceptionHandler.java
@@ -1,12 +1,15 @@
 package shopping.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class CommonExceptionHandler {
 
     @ExceptionHandler(WooWaException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String woowaExceptionHandler(WooWaException exception) {
         //todo: 향후 수정 예정
         return "error/error";
