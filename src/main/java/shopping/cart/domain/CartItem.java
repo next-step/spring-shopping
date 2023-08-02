3243b67182e0f3aa49f3946037fd23a6cbc0f39d
chanwooDev <pove2019@gmail.com>
2023-08-02 17:09:00 +0900

    feat: CartItem 생성, findAllDtoByMemberId 기능 추가

---
+++ src/main/java/shopping/cart/domain/CartItem.java
@@ -0,0 +1,56 @@
+package shopping.cart.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import shopping.product.domain.vo.Money;
+
+@Entity
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long memberId;
+    private Long productId;
+    private String productName;
+    private Money productPrice;
+    private int quantity;
+
+    protected CartItem() {
+    }
+
+    public CartItem(Long memberId, Long productId, String productName, Money productPrice,
+        int quantity) {
+        this.memberId = memberId;
+        this.productId = productId;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public Money getProductPrice() {
+        return productPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
