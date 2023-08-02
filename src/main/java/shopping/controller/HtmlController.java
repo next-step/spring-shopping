ca6f653fd2ca313d2d25648da873477f87f810f3
devxb <dlwnsdud205@naver.com>
2023-08-02 16:39:36 +0900

    step3: step3 기능을 구현한다

--- src/main/java/shopping/controller/HtmlController.java
+++ src/main/java/shopping/controller/HtmlController.java
@@ -2,14 +2,17 @@ package shopping.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login")
-public class LoginViewController {
+public class HtmlController {
 
-    @GetMapping
+    @GetMapping("/login")
     public String loginView() {
         return "login";
+    }
+
+    @GetMapping("/cart")
+    public String cartView() {
+        return "cart";
     }
 }
