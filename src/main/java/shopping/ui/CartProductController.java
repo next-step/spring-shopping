8bf4e3f0c8cd2923c41be8531b89a55c6a8512a2
pushedrumex <pushedrumex@naver.com>
2023-08-02 17:08:33 +0900

    feat: CartProductController.findCartProducts 메서드는 장바구니에 있는 물건을 반환한다

--- src/main/java/shopping/ui/CartProductController.java
+++ src/main/java/shopping/ui/CartProductController.java
@@ -1,13 +1,16 @@
 package shopping.ui;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import shopping.application.CartProductService;
+import shopping.dto.FindCartProductResponse;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 @RequestMapping("/cart/products")
@@ -17,6 +20,12 @@ public class CartProductController {
 
     public CartProductController(CartProductService cartProductService) {
         this.cartProductService = cartProductService;
+    }
+
+    @ResponseBody
+    @GetMapping
+    public List<FindCartProductResponse> findCartProducts(HttpServletRequest request) {
+        return cartProductService.findCartProducts((Long) request.getAttribute("memberId"));
     }
 
     @ResponseBody
