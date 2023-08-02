0b9acae4ae55007e881090d622674fc1ef9660e4
ProtoSeo <proto_seo@naver.com>
2023-08-02 17:52:07 +0900

    fix: CartItem 응답에 수량 추가
    Co-authored-by: impati <yongs170@naver.com>

--- src/main/java/shopping/dto/response/CartItemResponse.java
+++ src/main/java/shopping/dto/response/CartItemResponse.java
@@ -5,22 +5,29 @@ import shopping.domain.cart.CartItem;
 public class CartItemResponse {
 
     private final Long cartItemId;
+    private final int quantity;
     private final ProductResponse product;
 
-    protected CartItemResponse(final Long cartItemId, final ProductResponse product) {
+    protected CartItemResponse(final Long cartItemId, final int quantity, final ProductResponse product) {
         this.cartItemId = cartItemId;
+        this.quantity = quantity;
         this.product = product;
     }
 
     public static CartItemResponse from(final CartItem cartItem) {
         return new CartItemResponse(
                 cartItem.getId(),
+                cartItem.getQuantity(),
                 ProductResponse.from(cartItem.getProduct())
         );
     }
 
     public Long getCartItemId() {
         return this.cartItemId;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
     }
 
     public ProductResponse getProduct() {
