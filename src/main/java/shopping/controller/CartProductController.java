3100b5eb7c301fc14b75fc691f26760f82e0e02a
whatasame <whatasame@gmail.com>
2023-08-03 16:55:06 +0900

    feat: 웹 페이지 연동
    Co-authored-by: worldii <bbc2788@naver.com>

--- src/main/java/shopping/controller/CartProductController.java
+++ src/main/java/shopping/controller/CartProductController.java
@@ -1,34 +1,13 @@
 package shopping.controller;
 
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import shopping.auth.LoginUser;
-import shopping.dto.request.CartProductRequest;
-import shopping.service.CartProductService;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping("/cart")
 public class CartProductController {
 
-    private final CartProductService cartProductService;
-
-    public CartProductController(final CartProductService cartProductService) {
-        this.cartProductService = cartProductService;
-    }
-
-    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public ResponseEntity<Void> createCartProduct(
-        @LoginUser Long memberId,
-        @RequestBody final CartProductRequest request
-    ) {
-        cartProductService.createCartProduct(memberId, request);
-
-        return ResponseEntity.ok().build();
+    @GetMapping("/cart")
+    public String getAllCartProductsPage() {
+        return "cart";
     }
 }
