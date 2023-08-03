3e5f282e6b39afbf7be0e861a0492dfce592fdb3
gunner6603 <gunner6603@snu.ac.kr>
2023-08-03 14:02:22 +0900

    test: AuthInterceptor 테스트 추가

--- src/main/java/shopping/interceptor/AuthInterceptor.java
+++ src/main/java/shopping/interceptor/AuthInterceptor.java
@@ -1,16 +1,18 @@
 package shopping.interceptor;
 
 import java.util.Objects;
-import javax.naming.AuthenticationException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
+import shopping.exception.ErrorCode;
+import shopping.exception.ShoppingException;
 import shopping.utils.JwtProvider;
 
 public class AuthInterceptor implements HandlerInterceptor {
 
+    public static final String BEARER_TOKEN_TYPE = "Bearer ";
     private static Logger logger = LoggerFactory.getLogger(AuthInterceptor.class);
     private final JwtProvider jwtProvider;
 
@@ -25,16 +27,16 @@ public class AuthInterceptor implements HandlerInterceptor {
 
         final String header = request.getHeader("Authorization");
         if (Objects.isNull(header)) {
-            throw new AuthenticationException("등록된 사용자가 아닙니다");
+            throw new ShoppingException(ErrorCode.NO_AUTHENTICATION_HEADER);
         }
 
-        if (!header.startsWith("Bearer")) {
-            throw new AuthenticationException("지원하는 토큰 타입이 아닙니다");
+        if (!header.startsWith(BEARER_TOKEN_TYPE)) {
+            throw new ShoppingException(ErrorCode.INVALID_TOKEN_TYPE);
         }
 
-        String token = header.substring(7);
+        String token = header.substring(BEARER_TOKEN_TYPE.length());
         if (!jwtProvider.validate(token)) {
-            throw new AuthenticationException("유효하지 않은 토큰 입니다");
+            throw new ShoppingException(ErrorCode.INVALID_TOKEN);
         }
 
         Long userId = Long.valueOf(jwtProvider.parseToken(token));
