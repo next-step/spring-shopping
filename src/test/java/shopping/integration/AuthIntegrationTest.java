b7cec418f87757ff3e0cf03a0f76a50a2a9e3252
rimrim990 <lim990718@naver.com>
2023-08-03 20:47:14 +0900

    test: AuthIntegrationTest 에 로그인 테스트 케이스 추가

--- src/test/java/shopping/integration/AuthIntegrationTest.java
+++ src/test/java/shopping/integration/AuthIntegrationTest.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("로그인 기능")
-public class AuthIntegrationTest extends IntegrationTest {
+class AuthIntegrationTest extends IntegrationTest {
 
     @DisplayName("이메일과 비밀번호로 로그인을 한다")
     @Test
@@ -25,4 +25,31 @@ public class AuthIntegrationTest extends IntegrationTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 
+    @DisplayName("비밀번호가 일치하지 않으면 UNAUTHORIZED 상태를 반환한다.")
+    @Test
+    void returnStatusUnauthorized_WhenPasswordIsNotMatch() {
+        // given
+        String email = "woowa1@woowa.com";
+        String invalidPassword = "invalid";
+
+        // when
+        ExtractableResponse<Response> response = AuthIntegrationSupporter.login(email, invalidPassword);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    @DisplayName("이메일과 일치하는 사용자가 없으면 UNAUTHORIZED 상태를 반환한다.")
+    @Test
+    void returnStatus400_WhenPasswordIsNotMatch() {
+        // given
+        String email = "notExist@woowa.com";
+        String invalidPassword = "1234";
+
+        // when
+        ExtractableResponse<Response> response = AuthIntegrationSupporter.login(email, invalidPassword);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+    }
 }
