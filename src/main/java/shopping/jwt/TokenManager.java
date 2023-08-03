e9cc4b3d452253386df3fb5e1871109e88ec75f1
rimrim990 <lim990718@naver.com>
2023-08-03 19:52:59 +0900

    refactor: 비밀키 주입을 생성자로 받도록 수정

--- src/main/java/shopping/jwt/TokenManager.java
+++ src/main/java/shopping/jwt/TokenManager.java
@@ -15,8 +15,11 @@ public class TokenManager {
     private static final long TOKEN_VALIDITY_MILLI = 60 * 60 * 1000;
     public static final String MEMBER_ID_KEY = "memberId";
 
-    @Value("${shopping.token.secretKey}")
-    private String SECRET_KEY;
+    private final String secretKey;
+
+    public TokenManager(@Value("${shopping.token.secretKey}") String secretKey) {
+        this.secretKey = secretKey;
+    }
 
     public String createToken(Long id) {
         Date date = new Date();
@@ -25,14 +28,14 @@ public class TokenManager {
                 .setIssuedAt(date)
                 .setExpiration(new Date(date.getTime() + TOKEN_VALIDITY_MILLI))
                 .claim(MEMBER_ID_KEY, id)
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
     }
 
     public Long decodeToken(String token) {
         try {
             return Jwts.parser()
-                    .setSigningKey(SECRET_KEY)
+                    .setSigningKey(secretKey)
                     .parseClaimsJws(token)
                     .getBody()
                     .get(MEMBER_ID_KEY, Long.class);
