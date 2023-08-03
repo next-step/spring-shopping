f21c28fe2b08cb3be667ee8824922836f489cb0e
chanwooDev <pove2019@gmail.com>
2023-08-03 14:49:20 +0900

    feat: 인증,인가 인터셉터, custom resolver 구현

---
+++ src/main/java/shopping/auth/interceptor/LoginTokenInterceptor.java
@@ -0,0 +1,39 @@
+package shopping.auth.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import shopping.auth.TokenProvider;
+import shopping.exception.WooWaException;
+
+@Component
+public class LoginTokenInterceptor implements HandlerInterceptor {
+
+    public static final String MEMBER_KEY = "memberId";
+    private static final String BEARER_TYPE = "Bearer";
+    private final TokenProvider tokenProvider;
+
+    public LoginTokenInterceptor(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String value = request.getHeader("Authorization");
+
+        if (value == null) {
+            throw new WooWaException("권한이 없습니다", HttpStatus.UNAUTHORIZED);
+        }
+
+        String accessToken = value.substring(BEARER_TYPE.length()).trim();
+
+        if (tokenProvider.validateToken(accessToken)) {
+            throw new WooWaException("권한이 없습니다", HttpStatus.UNAUTHORIZED);
+        }
+
+        request.setAttribute(MEMBER_KEY, tokenProvider.getPayload(accessToken));
+        return true;
+    }
+}
---
+++ src/main/java/shopping/auth/resolver/AuthMemberResolver.java
@@ -0,0 +1,26 @@
+package shopping.auth.resolver;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import shopping.auth.annotation.AuthMember;
+import shopping.auth.interceptor.LoginTokenInterceptor;
+
+public class AuthMemberResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter methodParameter) {
+        return methodParameter.getParameterAnnotation(AuthMember.class) != null;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+        NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        HttpServletRequest request
+            = (HttpServletRequest) webRequest.getNativeRequest();
+        return request.getAttribute(LoginTokenInterceptor.MEMBER_KEY);
+    }
+}
