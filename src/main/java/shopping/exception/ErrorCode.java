f88df6f561c5119852808c5e4f2e3c9bdba8e95d
gunner6603 <gunner6603@snu.ac.kr>
2023-08-03 14:39:05 +0900

    test: CartIntegrationTest 테스트 추가

--- src/main/java/shopping/exception/ErrorCode.java
+++ src/main/java/shopping/exception/ErrorCode.java
@@ -10,7 +10,8 @@ public enum ErrorCode {
     NO_AUTHENTICATION_HEADER(HttpStatus.UNAUTHORIZED, "인증 정보가 없습니다."),
     INVALID_TOKEN_TYPE(HttpStatus.UNAUTHORIZED, "지원하는 토큰 타입이 아닙니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효한 토큰이 아닙니다."),
-    ;
+    DUPULICATE_CART_ITEM(HttpStatus.BAD_REQUEST, "장바구니에 이미 존재하는 상품입니다."),
+    INVALID_CART_ITEM_QUANTITY(HttpStatus.BAD_REQUEST, "장바구니 상품 수량은 1개 이상 1,000개 이하여야 합니다.");
 
     private final HttpStatus status;
     private final String message;
--- src/main/java/shopping/service/CartService.java
+++ src/main/java/shopping/service/CartService.java
@@ -11,6 +11,8 @@ import shopping.domain.entity.UserEntity;
 import shopping.dto.request.CartItemAddRequest;
 import shopping.dto.request.CartItemUpdateRequest;
 import shopping.dto.response.CartItemResponse;
+import shopping.exception.ErrorCode;
+import shopping.exception.ShoppingException;
 import shopping.repository.CartItemRepository;
 import shopping.repository.ProductRepository;
 import shopping.repository.UserRepository;
@@ -19,6 +21,8 @@ import shopping.repository.UserRepository;
 @Transactional(readOnly = true)
 public class CartService {
 
+    public static final int ITEM_MIN_QUANTITY = 1;
+    public static final int ITEM_MAX_QUANTITY = 1000;
     private final CartItemRepository cartItemRepository;
     private final UserRepository userRepository;
     private final ProductRepository productRepository;
@@ -34,8 +38,14 @@ public class CartService {
     @Transactional
     public void addCartItem(CartItemAddRequest cartItemAddRequest, Long userId) {
         final UserEntity userProxy = userRepository.getReferenceById(userId);
-        final ProductEntity product = productRepository.findById(cartItemAddRequest.getProductId())
+        final Long productId = cartItemAddRequest.getProductId();
+        final ProductEntity product = productRepository.findById(productId)
             .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 장바구니 상품입니다."));
+
+        if (cartItemRepository.existsByUserIdAndProductId(userId, productId)) {
+            throw new ShoppingException(ErrorCode.DUPULICATE_CART_ITEM);
+        }
+
         final CartItemEntity cartItemEntity = new CartItemEntity(userProxy, product);
         cartItemRepository.save(cartItemEntity);
     }
@@ -51,9 +61,16 @@ public class CartService {
         final CartItemUpdateRequest cartItemUpdateRequest, final Long userId) {
         final CartItemEntity cartItem = cartItemRepository.findById(cartItemId)
             .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 장바구니 상품입니다."));
+
         if (!Objects.equals(cartItem.getUser().getId(), userId)) {
             throw new IllegalArgumentException("유저의 장바구니 상품이 아닙니다.");
         }
+
+        if (cartItemUpdateRequest.getQuantity() < ITEM_MIN_QUANTITY
+            || ITEM_MAX_QUANTITY < cartItemUpdateRequest.getQuantity()) {
+            throw new ShoppingException(ErrorCode.INVALID_CART_ITEM_QUANTITY);
+        }
+
         cartItem.updateQuantity(cartItemUpdateRequest.getQuantity());
     }
 
