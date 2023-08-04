d17b8a76284b7f06ac12da0c8a8b07a664aaf919
devxb <dlwnsdud205@naver.com>
2023-08-04 13:53:44 +0900

    fix: ViewController 의 Bean 이름 중복 등록 버그를 수정한다

--- src/main/java/shopping/auth/controller/AuthViewController.java
+++ src/main/java/shopping/auth/controller/AuthViewController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ViewController {
-    
+public class AuthViewController {
+
     @GetMapping("/login")
     public String loginView() {
         return "login";
