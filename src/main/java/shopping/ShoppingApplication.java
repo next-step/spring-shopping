559dff9653fba509509b630546e57c41dd3a7e40
hwihwi <yjh200523@naver.com>
2023-08-04 14:54:40 +0900

    move: 패키지 구조 변경

--- src/main/java/shopping/ShoppingApplication.java
+++ src/main/java/shopping/ShoppingApplication.java
@@ -1,19 +1,16 @@
-package shopping.app;
+package shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EntityScan(basePackages = "shopping")
 @EnableJpaAuditing
-@SpringBootApplication(scanBasePackages = "shopping")
-@EnableJpaRepositories(basePackages = "shopping")
+@SpringBootApplication
+@EnableJpaRepositories
 public class ShoppingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShoppingApplication.class, args);
     }
-
 }
