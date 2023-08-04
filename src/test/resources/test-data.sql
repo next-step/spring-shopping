5d04094d01b8535923c8e3ce54b280fe58ac82df
whatasame <whatasame@gmail.com>
2023-08-04 16:21:20 +0900

    build: 운영 DB, 테스트 DB 분리
    Co-authored-by: worldii <bbc2788@naver.com>

---
+++ src/test/resources/test-data.sql
@@ -0,0 +1,11 @@
+insert into product(image, name, price)
+values ('/assets/img/chicken.png', '치킨', 20000),
+       ('/assets/img/hamburger.jpeg', '햄버거', 10000),
+       ('/assets/img/pizza.jpg', '피자', 20000);
+
+insert into members(email, password)
+values ('woowacamp@naver.com', 'woowacamp');
+
+insert into cart_products(member_id, product_id, quantity)
+values (1, 1, 3),
+       (1, 2, 1);
