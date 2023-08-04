6973ed735aebc8d923d3510e3fb504862ddd0e65
hwihwi <yjh200523@naver.com>
2023-08-04 15:25:03 +0900

    refactor: 용어 변경 및 매직 넘버 상수화

--- src/main/java/shopping/auth/infra/JwtUtils.java
+++ src/main/java/shopping/auth/infra/JwtUtils.java
@@ -11,19 +11,20 @@ import shopping.auth.domain.User;
 @Component
 public final class JwtUtils {
 
+    private static final int TOKEN_BEGIN_INDEX = 7;
     private final String secretKey;
-    private final long validityInMilliseconds;
+    private final long expire;
 
     JwtUtils(@Value("${security.jwt.token.secret-key:testKey}") String secretKey,
-            @Value("${security.jwt.token.expire-length:3000000}") long validityInMilliseconds) {
+            @Value("${security.jwt.token.expire-length:3000000}") long expire) {
         this.secretKey = secretKey;
-        this.validityInMilliseconds = validityInMilliseconds;
+        this.expire = expire;
     }
 
     public String generate(final User user) {
         Claims claims = Jwts.claims().setSubject(String.valueOf(user.getId()));
         Date now = new Date();
-        Date validity = new Date(now.getTime() + validityInMilliseconds);
+        Date validity = new Date(now.getTime() + expire);
 
         return Jwts.builder()
                 .setClaims(claims)
@@ -34,6 +35,7 @@ public final class JwtUtils {
     }
 
     public String payload(final String token) {
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token.substring(7)).getBody().getSubject();
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token.substring(TOKEN_BEGIN_INDEX)).getBody()
+                .getSubject();
     }
 }
