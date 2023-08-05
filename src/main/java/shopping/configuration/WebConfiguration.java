cb25a418494e84a2c62b86b7f99ac6356e8db371
dongjoo0-0 <kdj486347@gmail.com>
2023-08-05 16:59:22 +0900

    refactor: 필요없는 @EnableWebMvc 제거

--- src/main/java/shopping/configuration/WebConfiguration.java
+++ src/main/java/shopping/configuration/WebConfiguration.java
@@ -2,9 +2,7 @@ package shopping.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import shopping.auth.AuthArgumentResolver;
 import shopping.auth.AuthInterceptor;
@@ -13,23 +11,12 @@ import shopping.auth.TokenExtractor;
 import java.util.List;
 
 @Configuration
-@EnableWebMvc
 public class WebConfiguration implements WebMvcConfigurer {
 
     private final TokenExtractor tokenExtractor;
 
     public WebConfiguration(TokenExtractor tokenExtractor) {
         this.tokenExtractor = tokenExtractor;
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/css/**")
-                .addResourceLocations("classpath:/static/css/");
-        registry.addResourceHandler("/js/**")
-                .addResourceLocations("classpath:/static/js/");
-        registry.addResourceHandler("/assets/**")
-                .addResourceLocations("classpath:/static/assets/");
     }
 
     @Override
