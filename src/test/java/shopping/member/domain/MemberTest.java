2716d9d7437efef1da7de1756c23c6a4b13a5db7
jiwon choi <jjiwoning12@gmail.com>
2023-08-05 18:02:09 +0900

    test: Member password 테스트 happy case 추가

--- src/test/java/shopping/member/domain/MemberTest.java
+++ src/test/java/shopping/member/domain/MemberTest.java
@@ -10,13 +10,21 @@ import org.junit.jupiter.api.Test;
 class MemberTest {
 
     @Test
-    @DisplayName("비밀번호가 일치하지 않으면 false를 반환한다.")
-    void login() {
-        Member member = new Member("hello", "hello");
+    @DisplayName("비밀번호가 일치하면 true를 반환한다.")
+    void loginSuccess() {
+        String password = "hello";
+        Member member = new Member("hello", password);
 
-        Assertions.assertAll(
-            () -> assertThat(member.login("hello")).isTrue(),
-            () -> assertThat(member.login("bye")).isFalse()
-        );
+        assertThat(member.login(password)).isTrue();
+    }
+
+    @Test
+    @DisplayName("비밀번호가 일치하지 않으면 false를 반환한다.")
+    void loginFail() {
+        String password = "hello";
+        String failPassword = "bye";
+        Member member = new Member("hello", password);
+
+        assertThat(member.login(failPassword)).isFalse();
     }
 }
