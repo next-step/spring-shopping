7be9ade193d24ef00fa5165582b2f7c8e5d65a1a
whatasame <whatasame@gmail.com>
2023-08-05 18:23:16 +0900

    refactor: logger로 로깅하도록 변경

--- src/main/java/shopping/exception/GlobalExceptionHandler.java
+++ src/main/java/shopping/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package shopping.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,6 +10,8 @@ import shopping.exception.dto.ExceptionResponse;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @ExceptionHandler(ShoppingException.class)
     public ResponseEntity<ExceptionResponse> handleShoppingException(
@@ -29,7 +33,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ExceptionResponse> handleException(
         final Exception exception
     ) {
-        exception.printStackTrace();
+        logger.warn("{} = {}", exception.getClass().getSimpleName(), exception.getMessage());
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(new ExceptionResponse("서버 내부 예외가 발생했습니다."));
