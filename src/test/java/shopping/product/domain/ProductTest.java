deaa4e80b11865ea53a6482d26e2581e6033c02b
jiwon choi <jjiwoning12@gmail.com>
2023-08-05 18:02:24 +0900

    test: Product 생성 테스트 happy case 추가

--- src/test/java/shopping/product/domain/ProductTest.java
+++ src/test/java/shopping/product/domain/ProductTest.java
@@ -1,5 +1,7 @@
 package shopping.product.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,17 +13,23 @@ import shopping.exception.WooWaException;
 class ProductTest {
 
     @Test
+    @DisplayName("price가 양수면 정상적으로 생성된다.")
+    void create() {
+        Image image = createLocalImage();
+
+        assertThatCode(() -> new Product("name", image, "0"))
+            .doesNotThrowAnyException();
+    }
+
+    @Test
     @DisplayName("price가 마이너스면 IllegealException을 반환한다")
     void throwExceptionIfPriceIsMinus() {
         Image image = createLocalImage();
 
-        Assertions.assertThatThrownBy(() -> new Product("name", image, "-1"))
+        assertThatThrownBy(() -> new Product("name", image, "-1"))
             .isInstanceOf(WooWaException.class)
             .hasCause(new IllegalArgumentException())
             .hasMessage("상품의 가격은 0원보다 작을 수 없습니다.");
-
-        Assertions.assertThatCode(() -> new Product("name", image, "0"))
-            .doesNotThrowAnyException();
     }
 
     private static Image createLocalImage() {
