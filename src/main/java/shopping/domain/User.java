504c697ed0678ca32d27c2bd55f73244f1980cf7
jjungk <jsk33a@gmail.com>
2023-08-06 21:01:26 +0900

    refactor: User 도메인 추가

---
+++ src/main/java/shopping/domain/User.java
@@ -0,0 +1,35 @@
+package shopping.domain;
+
+import java.util.Objects;
+import shopping.domain.entity.UserEntity;
+import shopping.dto.request.LoginRequest;
+import shopping.exception.ErrorCode;
+import shopping.exception.ShoppingException;
+
+public class User {
+
+    private final Long id;
+    private final String email;
+    private final String password;
+
+    private User (final Long id, final String email, final String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public static User from(final UserEntity userEntity) {
+        return new User(
+            userEntity.getId(),
+            userEntity.getEmail(),
+            userEntity.getPassword()
+        );
+    }
+
+    public void validatePassword(final LoginRequest loginRequest) {
+        if (!Objects.equals(this.password, loginRequest.getPassword())) {
+            throw new ShoppingException(ErrorCode.INVALID_PASSWORD);
+        }
+    }
+
+}
--- src/main/java/shopping/service/UserService.java
+++ src/main/java/shopping/service/UserService.java
@@ -3,6 +3,7 @@ package shopping.service;
 import java.util.Objects;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import shopping.domain.User;
 import shopping.domain.entity.UserEntity;
 import shopping.dto.request.LoginRequest;
 import shopping.dto.response.LoginResponse;
@@ -26,15 +27,11 @@ public class UserService {
     public LoginResponse login(final LoginRequest loginRequest) {
         UserEntity userEntity = userRepository.findByEmail(loginRequest.getEmail())
             .orElseThrow(() -> new ShoppingException(ErrorCode.INVALID_EMAIL));
-        validatePassword(loginRequest, userEntity);
+        User user = User.from(userEntity);
+        user.validatePassword(loginRequest);
 
         final String accessToken = jwtProvider.generateToken(String.valueOf(userEntity.getId()));
         return LoginResponse.from(accessToken);
     }
 
-    private void validatePassword(final LoginRequest loginRequest, final UserEntity userEntity) {
-        if (!Objects.equals(loginRequest.getPassword(), userEntity.getPassword())) {
-            throw new ShoppingException(ErrorCode.INVALID_PASSWORD);
-        }
-    }
 }
