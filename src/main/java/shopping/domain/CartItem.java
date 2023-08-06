ac470a758a94975eade5dde5e5fd857d5949aefd
jjungk <jsk33a@gmail.com>
2023-08-06 20:51:42 +0900

    refactor: CartItem 도메인 추가

---
+++ src/main/java/shopping/domain/CartItem.java
@@ -0,0 +1,63 @@
+package shopping.domain;
+
+import java.util.Objects;
+import shopping.domain.entity.CartItemEntity;
+import shopping.exception.ErrorCode;
+import shopping.exception.ShoppingException;
+
+public final class CartItem {
+
+    private final static int QUANTITY_ZERO = 0;
+    public static final int ITEM_MIN_QUANTITY = 0;
+    public static final int ITEM_MAX_QUANTITY = 1000;
+
+    private final Long id;
+    private final Long userId;
+    private final Long productId;
+    private final int quantity;
+
+    private CartItem(final Long id, final Long userId, final Long productId, final int quantity) {
+        validateQuantity(quantity);
+        this.id = id;
+        this.userId = userId;
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public static CartItem from(final CartItemEntity cartItemEntity) {
+        return new CartItem(
+            cartItemEntity.getId(),
+            cartItemEntity.getUser().getId(),
+            cartItemEntity.getProduct().getId(),
+            cartItemEntity.getQuantity());
+    }
+
+    public static CartItem ofCartItemEntityAndQuantity(final CartItemEntity cartItemEntity, final int quantity) {
+        return new CartItem(
+            cartItemEntity.getId(),
+            cartItemEntity.getUser().getId(),
+            cartItemEntity.getProduct().getId(),
+            quantity);
+    }
+
+    private void validateQuantity(final int quantity) {
+        if (isOutOfBound(quantity)) {
+            throw new ShoppingException(ErrorCode.INVALID_CART_ITEM_QUANTITY);
+        }
+    }
+
+    private boolean isOutOfBound(final int quantity) {
+        return (quantity < ITEM_MIN_QUANTITY || ITEM_MAX_QUANTITY < quantity);
+    }
+
+    public void validateUser(final Long userId) {
+        if (!Objects.equals(this.userId, userId)) {
+            throw new ShoppingException(ErrorCode.INVALID_CART_ITEM);
+        }
+    }
+
+    public boolean isQuantityZero() {
+        return (this.quantity == QUANTITY_ZERO);
+    }
+
+}
--- src/main/java/shopping/service/CartService.java
+++ src/main/java/shopping/service/CartService.java
@@ -1,10 +1,10 @@
 package shopping.service;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import shopping.domain.CartItem;
 import shopping.domain.entity.CartItemEntity;
 import shopping.domain.entity.ProductEntity;
 import shopping.domain.entity.UserEntity;
@@ -20,10 +20,6 @@ import shopping.repository.UserRepository;
 @Service
 @Transactional(readOnly = true)
 public class CartService {
-
-    public static final int ITEM_MIN_QUANTITY = 1;
-    public static final int ITEM_MAX_QUANTITY = 1000;
-    public static final int QUANTITY_ZERO = 0;
 
     private final CartItemRepository cartItemRepository;
     private final UserRepository userRepository;
@@ -57,31 +53,25 @@ public class CartService {
     @Transactional
     public void updateCartItemQuantity(final Long cartItemId,
         final CartItemUpdateRequest cartItemUpdateRequest, final Long userId) {
-        final CartItemEntity cartItem = findCartItemEntityBy(cartItemId);
-        validateUserHasCartItem(userId, cartItem);
-
         final int updateQuantity = cartItemUpdateRequest.getQuantity();
+        final CartItemEntity cartItemEntity = findCartItemEntityBy(cartItemId);
 
-        if (updateQuantity == QUANTITY_ZERO) {
-            cartItemRepository.delete(cartItem);
+        CartItem cartItem = CartItem.ofCartItemEntityAndQuantity(cartItemEntity, updateQuantity);
+        cartItem.validateUser(userId);
+        if (cartItem.isQuantityZero()) {
+            cartItemRepository.delete(cartItemEntity);
             return;
         }
 
-        validateCartItemQuantity(updateQuantity);
-        cartItem.updateQuantity(updateQuantity);
+        cartItemEntity.updateQuantity(updateQuantity);
     }
 
     @Transactional
     public void removeCartItem(final Long cartItemId, final Long userId) {
-        final CartItemEntity cartItem = findCartItemEntityBy(cartItemId);
-        validateUserHasCartItem(userId, cartItem);
-        cartItemRepository.delete(cartItem);
-    }
-
-    private void validateUserHasCartItem(final Long userId, final CartItemEntity cartItem) {
-        if (!Objects.equals(cartItem.getUser().getId(), userId)) {
-            throw new ShoppingException(ErrorCode.INVALID_CART_ITEM);
-        }
+        final CartItemEntity cartItemEntity = findCartItemEntityBy(cartItemId);
+        final CartItem cartItem = CartItem.from(cartItemEntity);
+        cartItem.validateUser(userId);
+        cartItemRepository.delete(cartItemEntity);
     }
 
     private CartItemEntity findCartItemEntityBy(final Long cartItemId) {
@@ -100,14 +90,4 @@ public class CartService {
         }
     }
 
-    private void validateCartItemQuantity(
-        final int quantity) {
-        if (isOutOfBound(quantity)) {
-            throw new ShoppingException(ErrorCode.INVALID_CART_ITEM_QUANTITY);
-        }
-    }
-
-    private boolean isOutOfBound(final int quantity) {
-        return quantity < ITEM_MIN_QUANTITY || ITEM_MAX_QUANTITY < quantity;
-    }
 }
