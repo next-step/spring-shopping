18b378517623a39f21e1ee7ce4c7660a37fe15fe
rimrim990 <lim990718@naver.com>
2023-08-07 14:10:47 +0900

    fix: 로그인하지 않은 사용자는 @Login 을 사용하지 못하도록 검증 로직 추가

--- src/main/java/shopping/ui/argumentresolver/LoginArgumentResolver.java
+++ src/main/java/shopping/ui/argumentresolver/LoginArgumentResolver.java
@@ -1,5 +1,6 @@
 package shopping.ui.argumentresolver;
 
+import java.util.Objects;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -7,8 +8,12 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 import javax.servlet.http.HttpServletRequest;
+import shopping.exception.AuthException;
 
 public class LoginArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private static final String LOGIN_ATTRIBUTE_KEY = "memberId";
+
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         boolean hasLoginAnnotation = parameter.hasParameterAnnotation(Login.class);
@@ -20,6 +25,13 @@ public class LoginArgumentResolver implements HandlerMethodArgumentResolver {
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
-        return request.getAttribute("memberId");
+        validateRequest(request);
+        return request.getAttribute(LOGIN_ATTRIBUTE_KEY);
+    }
+
+    private static void validateRequest(HttpServletRequest request) {
+        if (Objects.isNull(request.getAttribute(LOGIN_ATTRIBUTE_KEY))) {
+            throw new AuthException("로그인이 필요한 서비스입니다");
+        }
     }
 }
