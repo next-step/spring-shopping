6c2c443c884fb24c2e8406a2b28044330e278dd2
rimrim990 <lim990718@naver.com>
2023-08-07 13:56:07 +0900

    fix: RuntimeException 처리를 위한 ExceptionHandler 구현

--- src/main/java/shopping/ui/advice/GlobalControllerAdvice.java
+++ src/main/java/shopping/ui/advice/GlobalControllerAdvice.java
@@ -14,16 +14,24 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(TokenException.class)
     ResponseEntity<ErrorResponse> catchTokenException(TokenException exception) {
-        return new ResponseEntity<>(new ErrorResponse(exception.getMessage()), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(new ErrorResponse(exception.getMessage()),
+            HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(AuthException.class)
     ResponseEntity<ErrorResponse> catchAuthException(AuthException exception) {
-        return new ResponseEntity<>(new ErrorResponse(exception.getMessage()), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(new ErrorResponse(exception.getMessage()),
+            HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(ShoppingException.class)
     ResponseEntity<ErrorResponse> catchShoppingException(ShoppingException exception) {
         return ResponseEntity.badRequest().body(new ErrorResponse(exception.getMessage()));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    ResponseEntity<ErrorResponse> catchRuntimeException() {
+        return ResponseEntity.internalServerError()
+            .body(new ErrorResponse("알 수 없는 에러가 발생하였습니다."));
     }
 }
