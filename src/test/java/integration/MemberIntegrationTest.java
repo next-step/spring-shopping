42ee02fe7a71df88daa2c437b9bd12ce63eb7936
chanwooDev <pove2019@gmail.com>
2023-08-07 19:35:05 +0900

    test: 로그인 실패 통합테스트 추가
    - 일치하지 않는 email로 로그인을 실패한다
    - 일치하지 않는 password로 로그인을 실패한다

--- src/test/java/integration/MemberIntegrationTest.java
+++ src/test/java/integration/MemberIntegrationTest.java
@@ -1,12 +1,13 @@
 package integration;
 
 import static integration.helper.CommonRestAssuredUtils.post;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import shopping.exception.ErrorResponse;
 import shopping.member.dto.LoginRequest;
 import shopping.member.dto.LoginResponse;
 
@@ -20,9 +21,38 @@ class MemberIntegrationTest extends IntegrationTest {
         ExtractableResponse<Response> response =
             post("/login", new LoginRequest("hello@woowa.com", "hello1"));
 
-        Assertions.assertThat(response.statusCode()).isEqualTo(200);
-        Assertions.assertThat(response.body().as(LoginResponse.class))
+        assertThat(response.statusCode()).isEqualTo(200);
+        assertThat(response.body().as(LoginResponse.class))
             .extracting("accessToken")
             .isNotNull();
+    }
+
+    //일치하지 않는 email로 로그인을 실패한다
+    @Test
+    @DisplayName("일치하지 않는 email로 로그인을 실패한다")
+    void loginFail2() {
+        // given from test_data.sql
+
+        ExtractableResponse<Response> response =
+            post("/login", new LoginRequest("fail", "hello1"));
+
+        assertThat(response.statusCode()).isEqualTo(400);
+        assertThat(response.body().as(ErrorResponse.class)).extracting("message")
+            .isEqualTo("로그인 정보가 일치하지 않습니다.");
+        assertThat(response.body().as(ErrorResponse.class)).extracting("statusCode").isEqualTo(400);
+    }
+
+    @Test
+    @DisplayName("일치하지 않는 password로 로그인을 실패한다")
+    void loginFail() {
+        // given from test_data.sql
+
+        ExtractableResponse<Response> response =
+            post("/login", new LoginRequest("hello@woowa.com", "fail"));
+
+        assertThat(response.statusCode()).isEqualTo(400);
+        assertThat(response.body().as(ErrorResponse.class)).extracting("message")
+            .isEqualTo("로그인 정보가 일치하지 않습니다.");
+        assertThat(response.body().as(ErrorResponse.class)).extracting("statusCode").isEqualTo(400);
     }
 }
