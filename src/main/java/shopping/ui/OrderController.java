384d320ae472dcb9f868331327ac55e69967b91a
rimrim990 <lim990718@naver.com>
2023-08-08 20:05:37 +0900

    feat: 주문하기 페이지 연동

--- src/main/java/shopping/ui/OrderController.java
+++ src/main/java/shopping/ui/OrderController.java
@@ -3,6 +3,9 @@ package shopping.ui;
 import java.net.URI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import shopping.application.OrderService;
@@ -25,5 +28,11 @@ public class OrderController {
         return ResponseEntity
             .created(URI.create("/order-detail/" + response.getId()))
             .build();
+    }
+
+    @GetMapping("/order-detail/{id}")
+    public String orderDetail(@PathVariable Long id, Model model) {
+        model.addAttribute("orderId", id);
+        return "order-detail";
     }
 }
