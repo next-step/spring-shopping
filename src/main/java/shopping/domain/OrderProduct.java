4e240e9f6900dc5146245f635986be68fcc99983
pushedrumex <pushedrumex@naver.com>
2023-08-09 00:55:24 +0900

    feat: OrderProduct 생성자는 OrderProduct 객체를 생성한다

--- src/main/java/shopping/domain/OrderProduct.java
+++ src/main/java/shopping/domain/OrderProduct.java
@@ -1,5 +1,7 @@
 package shopping.domain;
 
+import java.text.MessageFormat;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -8,9 +10,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import shopping.exception.OrderProductException;
 
 @Entity
 public class OrderProduct {
+
+    public static final int MIN_PRODUCT_QUANTITY = 1;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,6 +37,9 @@ public class OrderProduct {
     }
 
     public OrderProduct(Long id, Order order, Product product, int quantity) {
+        validateOrder(order);
+        validateProduct(product);
+        validateQuantity(quantity);
         this.id = id;
         this.order = order;
         this.product = product;
@@ -41,4 +49,26 @@ public class OrderProduct {
     public OrderProduct(Order order, Product product, int quantity) {
         this(null, order, product, quantity);
     }
+
+    private void validateQuantity(int quantity) {
+        if (quantity < MIN_PRODUCT_QUANTITY) {
+            throw new OrderProductException(
+                MessageFormat
+                    .format("상품의 개수는 최소 {0}개여야합니다 현재 개수 : {1}개", MIN_PRODUCT_QUANTITY, quantity)
+            );
+        }
+    }
+
+    private void validateProduct(Product product) {
+        if (Objects.isNull(product)) {
+            throw new OrderProductException("product 가 존재하지 않습니다");
+        }
+    }
+
+    private void validateOrder(Order order) {
+        if (Objects.isNull(order)) {
+            throw new OrderProductException("order 가 존재하지 않습니다");
+        }
+    }
+
 }
