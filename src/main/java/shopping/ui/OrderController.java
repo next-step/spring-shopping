9b6ae7ea47622ca72224b2ef992c7891d7c947b2
rimrim990 <lim990718@naver.com>
2023-08-08 19:27:11 +0900

    refactor: order 상세 정보 페이지의 url 수정 및 컨트롤러 빈 등록

--- src/main/java/shopping/ui/OrderController.java
+++ src/main/java/shopping/ui/OrderController.java
@@ -2,11 +2,14 @@ package shopping.ui;
 
 import java.net.URI;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import shopping.application.OrderService;
 import shopping.dto.response.OrderResponse;
 import shopping.ui.argumentresolver.Login;
 
+@Controller
 public class OrderController {
 
     private final OrderService orderService;
@@ -16,10 +19,11 @@ public class OrderController {
     }
 
     @PostMapping("/order")
+    @ResponseBody
     public ResponseEntity<Void> order(@Login Long memberId) {
         OrderResponse response = orderService.order(memberId);
         return ResponseEntity
-            .created(URI.create("/order-history/" + response.getId()))
+            .created(URI.create("/order-detail/" + response.getId()))
             .build();
     }
 }
