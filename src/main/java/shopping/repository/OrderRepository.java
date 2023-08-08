dfe4d9c7d2dfd738b9bb5d9fd6b5c78888534c4d
rimrim990 <lim990718@naver.com>
2023-08-08 17:11:08 +0900

    feat: OrderRepository.save 구현

--- src/main/java/shopping/repository/OrderRepository.java
+++ src/main/java/shopping/repository/OrderRepository.java
@@ -1,11 +1,19 @@
 package shopping.repository;
 
+import javax.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
 import shopping.domain.Order;
 
 @Repository
 public class OrderRepository {
 
+    private final EntityManager entityManager;
+
+    public OrderRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     public void save(Order order) {
+        entityManager.persist(order);
     }
 }
