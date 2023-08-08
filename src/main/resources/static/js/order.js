946fb573769461cce65033f0cfcfb2ecf8c9ee58
impati <yongs170@naver.com>
2023-08-08 14:38:03 +0900

    feat: 주문 기능 페이지 구현

--- src/main/resources/static/js/order.js
+++ src/main/resources/static/js/order.js
@@ -6,16 +6,16 @@ const requestOrder = () => {
         return;
     }
 
-    // TODO: [4단계] 장바구니 아이템 추가 스펙에 맞게 변경
-    fetch('', {
-        method: '',
+    fetch('/orders', {
+        method: 'POST',
         headers: {
             'Authorization': `Bearer ${credentials}`,
             'Content-Type': 'application/json'
         }
     }).then((response) => {
-        // TODO: [4단계] 주문이 성공하면 주문 상세 페이지로 이동 (order.html 사용)
-        window.location.href = '';
+        return response.json();
+    }).then((data) => {
+        window.location.href = '/order-detail/' + data.orderId;
     }).catch((error) => {
         console.error(error);
     });
