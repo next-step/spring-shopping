f8027512ad0ef87f4188146141ca552dacd9b81d
jjungk <jsk33a@gmail.com>
2023-08-08 21:27:38 +0900

    refactor: OrderEntity 양방향 맵핑 제거

--- src/main/java/shopping/domain/entity/OrderEntity.java
+++ src/main/java/shopping/domain/entity/OrderEntity.java
@@ -19,24 +19,19 @@ public class OrderEntity {
     private Long totalPrice;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "cart_item_id")
-    private CartItemEntity cartItem;
-
-    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private UserEntity user;
 
     protected OrderEntity() {};
 
-    public OrderEntity(Long id, Long totalPrice, CartItemEntity cartItem, UserEntity user) {
+    public OrderEntity(Long id, Long totalPrice, UserEntity user) {
         this.id = id;
         this.totalPrice = totalPrice;
-        this.cartItem = cartItem;
         this.user = user;
     }
 
-    public OrderEntity(Long totalPrice, CartItemEntity cartItem, UserEntity user) {
-        this(null, totalPrice, cartItem, user);
+    public OrderEntity(Long totalPrice, UserEntity user) {
+        this(null, totalPrice, user);
     }
 
     public Long getId() {
@@ -45,10 +40,6 @@ public class OrderEntity {
 
     public Long getTotalPrice() {
         return totalPrice;
-    }
-
-    public CartItemEntity getCartItem() {
-        return cartItem;
     }
 
     public UserEntity getUser() {
