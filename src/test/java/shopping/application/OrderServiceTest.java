1bf207e8e47d0d4060f2d9374bb2b25b06ddd5fc
김명준 <audwnswmf@naver.com>
2023-08-08 15:25:35 +0900

    chore: 안쓰이는 로거 제거

--- src/test/java/shopping/application/OrderServiceTest.java
+++ src/test/java/shopping/application/OrderServiceTest.java
@@ -8,8 +8,6 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
@@ -54,8 +52,6 @@ class OrderServiceTest {
 
     @Autowired
     private OrderItemRepository orderItemRepository;
-
-    private Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Nested
     @DisplayName("주문 생성 서비스 테스트")
