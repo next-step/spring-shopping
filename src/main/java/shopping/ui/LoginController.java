acb531c89763f91c5ad9f465df22d02c409060f5
dahyen0o <seeunchoi99@gmail.com>
2023-08-09 19:29:20 +0900

    refactor: LoginController를 RestController로 수정

--- src/main/java/shopping/ui/LoginController.java
+++ src/main/java/shopping/ui/LoginController.java
@@ -2,15 +2,15 @@ package shopping.ui;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import shopping.application.AuthService;
 import shopping.dto.LoginRequest;
 import shopping.dto.LoginResponse;
 
-@Controller
+@RestController
 @RequestMapping("/login")
 public class LoginController {
 
