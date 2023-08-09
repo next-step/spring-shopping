8d925055a470f30798c7bcc503dfd86d5a9c0fa4
worldii <bbc2788@naver.com>
2023-08-09 15:21:58 +0900

    Chore : 패키지 정리

--- src/main/java/shopping/cart/domain/vo/Price.java
+++ src/main/java/shopping/cart/domain/vo/Price.java
@@ -1,4 +1,4 @@
-package shopping.global.vo;
+package shopping.cart.domain.vo;
 
 import java.util.Objects;
 import javax.persistence.Embeddable;
--- src/test/java/shopping/domain/PriceTest.java
+++ src/test/java/shopping/domain/PriceTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import shopping.global.vo.Price;
+import shopping.cart.domain.vo.Price;
 import shopping.global.exception.ShoppingException;
 
 @DisplayName("상품 가격 테스트")
@@ -28,6 +28,7 @@ class PriceTest {
     @ValueSource(ints = {-1, 0})
     @DisplayName("상품 가격이 0이하 인 경우, ShoppingException을 던진다.")
     void createProductPriceFailWithLessThanEqualZero(final int value) {
+        // when & then
         assertThatCode(() -> new Price(value))
             .isInstanceOf(ShoppingException.class)
             .hasMessage("상품 가격은 0이하면 안됩니다. 입력값: " + value);
