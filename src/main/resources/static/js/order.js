354bef6bc2b3ce0b42a571696240378031d069ff
jiwon choi <jjiwoning12@gmail.com>
2023-08-09 16:58:42 +0900

    refactor: 미션 요구 사항에 맞춰 프론트 코드 수정

--- src/main/resources/static/js/order.js
+++ src/main/resources/static/js/order.js
@@ -1,4 +1,4 @@
-const requestOrder = () => {
+const requestOrder = (cartItemIds) => {
     const credentials = sessionStorage.getItem('accessToken');
     if (!credentials) {
         alert('사용자 정보가 없습니다.');
@@ -7,15 +7,16 @@ const requestOrder = () => {
     }
 
     // TODO: [4단계] 장바구니 아이템 추가 스펙에 맞게 변경
-    fetch('', {
-        method: '',
+    fetch('/api/order', {
+        method: 'post',
         headers: {
             'Authorization': `Bearer ${credentials}`,
             'Content-Type': 'application/json'
-        }
-    }).then((response) => {
-        // TODO: [4단계] 주문이 성공하면 주문 상세 페이지로 이동 (order.html 사용)
-        window.location.href = '';
+        },
+        body: JSON.stringify({cartItemIds})
+    })
+    .then(response => {
+        window.location.href = `${response.headers.get("Location")}`;
     }).catch((error) => {
         console.error(error);
     });
