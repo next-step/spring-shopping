656f031367a85409f4d05408aacfa78ef1274686
worldii <bbc2788@naver.com>
2023-08-09 22:56:34 +0900

    Test : Money VO 유효성 검증

---
+++ src/test/java/shopping/domain/order/MoneyTest.java
@@ -0,0 +1,36 @@
+package shopping.domain.order;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import shopping.global.exception.ShoppingException;
+import shopping.order.domain.vo.Money;
+
+@DisplayName("가격 테스트")
+class MoneyTest {
+
+    @Test
+    @DisplayName("가격을 생성한다.")
+    void createMoney() {
+        /* given */
+        final int value = 1000;
+
+        /* when & then */
+        assertThatCode(() -> new Money(value))
+            .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("가격이 0이하 인 경우, ShoppingException을 던진다.")
+    void createMoneyFailWithLessThanZero() {
+        // when & then
+        int value = -1;
+        assertThatCode(() -> new Money(value))
+            .isInstanceOf(ShoppingException.class)
+            .hasMessage("주문 가격은 0이하면 안됩니다. 입력값: " + value);
+    }
+
+}
