ed0e5b1cbb1b506a45b44d6fa52d7f1b66a57bbe
jjungk <jsk33a@gmail.com>
2023-08-09 15:09:37 +0900

    remove: OrderProductEntity 객체 삭제

--- src/main/java/shopping/repository/OrderProductRepository.java
+++
@@ -1,8 +0,0 @@
-package shopping.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import shopping.domain.entity.OrderProductEntity;
-
-public interface OrderProductRepository extends JpaRepository<OrderProductEntity, Long> {
-
-}
