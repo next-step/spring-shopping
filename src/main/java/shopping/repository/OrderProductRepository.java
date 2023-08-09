d9ae4ae43f546013889a8ece3a847ad3355f6333
pushedrumex <pushedrumex@naver.com>
2023-08-09 21:03:44 +0900

    refactor: 사용하지 않는 메서드를 제거한다

--- src/main/java/shopping/repository/OrderProductRepository.java
+++ src/main/java/shopping/repository/OrderProductRepository.java
@@ -1,9 +1,7 @@
 package shopping.repository;
 
-import java.util.List;
 import javax.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
-import shopping.domain.OrderProduct;
 
 @Repository
 public class OrderProductRepository {
@@ -12,9 +10,5 @@ public class OrderProductRepository {
 
     public OrderProductRepository(EntityManager entityManager) {
         this.entityManager = entityManager;
-    }
-
-    public void saveAll(List<OrderProduct> orderProducts) {
-        orderProducts.forEach(entityManager::persist);
     }
 }
