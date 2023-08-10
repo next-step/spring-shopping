b313a5100183926771aa1b46ed7e4adc49a7be70
pushedrumex <pushedrumex@naver.com>
2023-08-11 02:21:43 +0900

    feat: CurrencyManager.getExchangeRate 는 환율 정보를 반환한다

---
+++ src/main/java/shopping/currency/CurrencyLayerResponse.java
@@ -0,0 +1,19 @@
+package shopping.currency;
+
+import java.util.Map;
+
+public class CurrencyLayerResponse {
+
+    private Map<String, Double> quotes;
+
+    private CurrencyLayerResponse() {
+    }
+
+    public CurrencyLayerResponse(Map<String, Double> quotes) {
+        this.quotes = quotes;
+    }
+
+    public Map<String, Double> getQuotes() {
+        return quotes;
+    }
+}
---
+++ src/test/java/shopping/currency/CurrencyManagerTest.java
@@ -0,0 +1,94 @@
+package shopping.currency;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchException;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import shopping.exception.CurrencyLayerException;
+
+@DisplayName("CurrencyManager 클래스")
+@ExtendWith(MockitoExtension.class)
+public class CurrencyManagerTest {
+
+    @InjectMocks
+    private CurrencyManager currencyManager;
+
+    @Mock
+    private ApiService apiService;
+
+    @Nested
+    @DisplayName("getExchangeRate 메서드는")
+    class GetExchangeRate_Method {
+
+        @Test
+        @DisplayName("base 에 해당하는 target 의 환율을 반환한다")
+        void returnExchangeRate() {
+            // given
+            String base = "USD";
+            String target = "KRW";
+            double exchangeRate = 1300.0;
+
+            Map<String, Double> quotes = new HashMap<>();
+            quotes.put(base + target, exchangeRate);
+
+            when(apiService.getResult(anyString(), eq(CurrencyLayerResponse.class)))
+                .thenReturn(new CurrencyLayerResponse(quotes));
+
+            // when
+            double result = currencyManager.getExchangeRate(base, target);
+
+            // then
+            assertThat(result).isEqualTo(exchangeRate);
+        }
+
+        @Test
+        @DisplayName("quotes 가 존재하지 않을 경우 CurrencyLayerException 을 던진다")
+        void throwCurrencyLayerException_WhenQuotesIsNull() {
+            // given
+            String base = "USD";
+            String target = "KRW";
+
+            when(apiService.getResult(anyString(), eq(CurrencyLayerResponse.class)))
+                .thenReturn(new CurrencyLayerResponse(null));
+
+            // when
+            Exception exception = catchException(
+                () -> currencyManager.getExchangeRate(base, target));
+
+            // then
+            assertThat(exception).isInstanceOf(CurrencyLayerException.class);
+            assertThat(exception.getMessage()).contains("quotes 정보를 불러오지 못했습니다");
+        }
+
+        @Test
+        @DisplayName("base 에 해당하는 target 환율 정보가 없을 경우 CurrencyLayerException 을 던진다")
+        void throwCurrencyLayerException_WhenRateNull() {
+            // given
+            String base = "USD";
+            String target = "KRW";
+
+            when(apiService.getResult(anyString(), eq(CurrencyLayerResponse.class)))
+                .thenReturn(new CurrencyLayerResponse(new HashMap<>()));
+
+            // when
+            Exception exception = catchException(
+                () -> currencyManager.getExchangeRate(base, target));
+
+            // then
+            assertThat(exception).isInstanceOf(CurrencyLayerException.class);
+            assertThat(exception.getMessage()).contains("환율 정보를 불러오지 못했습니다");
+        }
+
+    }
+}
