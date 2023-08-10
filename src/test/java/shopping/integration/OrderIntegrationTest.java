2c8424be6ef929ba80418c94a61fba211d38843f
jjungk <jsk33a@gmail.com>
2023-08-10 18:14:52 +0900

    feat: 장바구니에 상품이 없을 경우 주문 불가 예외 처리 추가

--- src/test/java/shopping/integration/OrderIntegrationTest.java
+++ src/test/java/shopping/integration/OrderIntegrationTest.java
@@ -1,17 +1,21 @@
 package shopping.integration;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import shopping.TestHelper;
+import shopping.dto.request.CartItemAddRequest;
 import shopping.dto.request.LoginRequest;
 import shopping.dto.response.LoginResponse;
+import shopping.exception.ErrorCode;
+import shopping.exception.ErrorResponse;
 
 @DisplayName("OrderIntegrationTest")
 public class OrderIntegrationTest extends IntegrationTest{
@@ -26,7 +30,32 @@ public class OrderIntegrationTest extends IntegrationTest{
     @DisplayName("성공 : 장바구니 상품 주문 완료")
     void successOrder() {
         // given
-        final LoginRequest loginRequest = new LoginRequest("test_email@woowafriends.com",
+        LoginRequest loginRequest = new LoginRequest("test_email@woowafriends.com",
+            "test_password!");
+        String accessToken = TestHelper.login(loginRequest).as(LoginResponse.class)
+            .getAccessToken();
+        CartItemAddRequest cartItemAddRequest = new CartItemAddRequest(1L);
+        TestHelper.addCartItem(accessToken, cartItemAddRequest);
+
+        // when
+        ExtractableResponse<Response> response = RestAssured
+            .given().log().all()
+            .auth().oauth2(accessToken)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .post("/order")
+            .then()
+            .log().all().extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    @Test
+    @DisplayName("실패 : 장바구니에 상품이 없다면 주문 불가")
+    void failOrder() {
+        // given
+        LoginRequest loginRequest = new LoginRequest("test_email@woowafriends.com",
             "test_password!");
         String accessToken = TestHelper.login(loginRequest).as(LoginResponse.class)
             .getAccessToken();
@@ -42,7 +71,8 @@ public class OrderIntegrationTest extends IntegrationTest{
             .log().all().extract();
 
         // then
-        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        ErrorResponse errorResponse = response.as(ErrorResponse.class);
+        assertThat(errorResponse.getErrorCode()).isEqualTo(ErrorCode.INVALID_PURCHASE);
     }
 
 }
