369a308c28d8b810a26f8e700bcd8de84d0e81d3
devxb <dlwnsdud205@naver.com>
2023-08-10 12:29:31 +0900

    test: Mart repository 모듈 테스트를 추가한다

--- mart/mart-repository/src/test/java/shopping/mart/repository/ProductPersistServiceTest.java
+++ mart/mart-repository/src/test/java/shopping/mart/repository/ProductPersistServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import java.util.Optional;
+import org.assertj.core.api.recursive.comparison.RecursiveComparisonConfiguration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -16,7 +17,7 @@ import shopping.mart.app.domain.Product;
 class ProductPersistServiceTest extends JpaTest {
 
     @Autowired
-    private ProductPersistService productRepository;
+    private ProductPersistService productPersistService;
 
     private void assertProduct(final Optional<Product> result, Product product) {
         assertThat(result).isPresent();
@@ -36,10 +37,10 @@ class ProductPersistServiceTest extends JpaTest {
         @DisplayName("이름에 해당하는 Product를 반환한다.")
         void it_return_name_matched_product() {
             // given
-            productRepository.saveProduct(product);
+            productPersistService.saveProduct(product);
 
             // when
-            Optional<Product> result = productRepository.findByProductName(product.getName());
+            Optional<Product> result = productPersistService.findByProductName(product.getName());
 
             // then
             assertProduct(result);
@@ -49,7 +50,7 @@ class ProductPersistServiceTest extends JpaTest {
         @DisplayName("이름에 해당하는 product가 없으면, Optional.empty를 반환한다.")
         void it_return_empty_optional_when_not_exists_product() {
             // when
-            Optional<Product> result = productRepository.findByProductName(product.getName());
+            Optional<Product> result = productPersistService.findByProductName(product.getName());
 
             // then
             assertThat(result).isNotPresent();
@@ -62,6 +63,7 @@ class ProductPersistServiceTest extends JpaTest {
             assertThat(result.get().getImageUrl()).isEqualTo(product.getImageUrl());
             assertThat(result.get().getPrice()).isEqualTo(product.getPrice());
         }
+
     }
 
     @Nested
@@ -75,11 +77,11 @@ class ProductPersistServiceTest extends JpaTest {
             Product pizza = new Product("피자", "/default-product.png", "10000");
             Product beer = new Product("맥주", "/default-product.png", "20000");
 
-            productRepository.saveProduct(pizza);
-            productRepository.saveProduct(beer);
+            productPersistService.saveProduct(pizza);
+            productPersistService.saveProduct(beer);
 
             // when
-            List<Product> result = productRepository.findAllProducts();
+            List<Product> result = productPersistService.findAllProducts();
 
             // then
             assertProduct(result, pizza, beer);
@@ -89,7 +91,7 @@ class ProductPersistServiceTest extends JpaTest {
         @DisplayName("저장된 product가 없을 경우 빈 리스트를 반환한다.")
         void it_return_empty_list_when_not_exists_products() {
             // when
-            List<Product> result = productRepository.findAllProducts();
+            List<Product> result = productPersistService.findAllProducts();
 
             // then
             assertThat(result).isEmpty();
@@ -108,5 +110,46 @@ class ProductPersistServiceTest extends JpaTest {
             assertThat(result.getImageUrl()).isEqualTo(expected.getImageUrl());
             assertThat(result.getPrice()).isEqualTo(expected.getPrice());
         }
+
     }
+
+    @Nested
+    @DisplayName("findById 메소드는")
+    class findById_method {
+
+        private final RecursiveComparisonConfiguration ignoreIdField = RecursiveComparisonConfiguration.builder()
+                .withIgnoredFields("id")
+                .build();
+
+        @Test
+        @DisplayName("productId 에 해당하는 product를 찾을 수 없으면, Optional.empty를 반환한다.")
+        void throw_empty_optional_when_cannot_find_matched_product() {
+            // given
+            long productId = 1L;
+
+            // when
+            Optional<Product> result = productPersistService.findById(productId);
+
+            // then
+            assertThat(result).isEmpty();
+        }
+
+        @Test
+        @DisplayName("productId 에 해당하는 product를 찾을 수 있으면, Product를 반환한다.")
+        void return_product_when_find_matched_product() {
+            // given
+            long productId = 1L;
+            Product expected = new Product("제육덮밥", "/default-product.png", "10000");
+            productPersistService.saveProduct(expected);
+
+            // when
+            Optional<Product> result = productPersistService.findById(productId);
+
+            // then
+            assertThat(result).isPresent();
+            assertThat(result.get()).usingRecursiveComparison(ignoreIdField).isEqualTo(expected);
+        }
+
+    }
+
 }
