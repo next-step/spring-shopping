d3d076653a4a4a5e0442f45a0bb96f2c817d6a4a
ProtoSeo <proto_seo@naver.com>
2023-08-10 10:38:05 +0900

    refactor: 사용하지 않는 메서드 삭제

--- src/main/java/shopping/repository/CartItemRepository.java
+++ src/main/java/shopping/repository/CartItemRepository.java
@@ -5,17 +5,11 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shopping.domain.cart.CartItem;
-import shopping.domain.member.Member;
-import shopping.domain.product.Product;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
     @EntityGraph(attributePaths = {"product", "member"})
     Optional<CartItem> findOneWithProductAndMemberById(final Long cartItemId);
-
-    CartItem getByMemberAndProduct(final Member member, final Product product);
-
-    boolean existsByMemberAndProduct(final Member member, final Product product);
 
     @EntityGraph(attributePaths = {"product"})
     List<CartItem> findAllByMemberId(final Long memberId);
