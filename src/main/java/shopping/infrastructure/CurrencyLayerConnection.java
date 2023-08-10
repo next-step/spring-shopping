f460c24a562a43d3a8cc703ff1f498b5fabdeb0e
dongjoo0-0 <kdj486347@gmail.com>
2023-08-10 14:45:45 +0900

    refactor: retry 기능 추가

--- src/main/java/shopping/infrastructure/CurrencyLayerConnection.java
+++ src/main/java/shopping/infrastructure/CurrencyLayerConnection.java
@@ -3,13 +3,19 @@ package shopping.infrastructure;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.util.retry.Retry;
 import shopping.dto.api.ExchangeRateResponse;
 import shopping.dto.web.response.ExchangeRateErrorResponse;
 import shopping.exception.infrastructure.ConnectionErrorException;
 import shopping.exception.infrastructure.NullResponseException;
 
+import java.time.Duration;
+
 @Component
 public class CurrencyLayerConnection implements ExchangeRateFetcher {
+
+    private static final int MAX_RETRY_ATTEMPTS = 3;
+    private static final Duration RETRY_DELAY = Duration.ofMillis(500);
 
     @Value("${security.currency-layer.access-key}")
     private String accessKey;
@@ -38,6 +44,7 @@ public class CurrencyLayerConnection implements ExchangeRateFetcher {
                         .build())
                 .retrieve()
                 .bodyToMono(ExchangeRateResponse.class)
+                .retryWhen(Retry.fixedDelay(MAX_RETRY_ATTEMPTS, RETRY_DELAY))
                 .block();
         validateNotNullResponse(response);
         return response;
