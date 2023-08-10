283bd0973be431cecbf7a3c943e60a8690c5437e
ProtoSeo <proto_seo@naver.com>
2023-08-10 10:33:04 +0900

    feat: 주문, 주문 상품 관련 스키마 작성

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,5 +1,7 @@
 drop table if exists CART_ITEM;
 drop table if exists PRODUCT;
+drop table if exists ORDER_ITEM;
+drop table if exists ORDERS;
 drop table if exists MEMBER;
 
 create table if not exists PRODUCT
@@ -29,4 +31,24 @@ create table if not exists CART_ITEM
     primary key(cart_item_id),
     foreign key (member_id) references MEMBER(member_id),
     foreign key (product_id) references PRODUCT(product_id)
+);
+
+create table if not exists ORDERS
+(
+    order_id bigint auto_increment not null,
+    member_id bigint not null,
+    primary key(order_id),
+    foreign key (member_id) references MEMBER(member_id)
+);
+
+create table if not exists ORDER_ITEM
+(
+    order_item_id bigint auto_increment not null,
+    order_id bigint not null,
+    image varchar(255) not null,
+    name varchar(30) not null,
+    price int not null,
+    quantity int not null,
+    primary key(order_item_id),
+    foreign key (order_id) references ORDERS(order_id)
 );
