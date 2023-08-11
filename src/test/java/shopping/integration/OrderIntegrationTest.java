be56c2f09c5599e95c3d9781fc2c81d9f408d785
rimrim990 <lim990718@naver.com>
2023-08-11 17:30:05 +0900

    test: 외부 API mocking

--- src/test/java/shopping/integration/OrderIntegrationTest.java
+++ src/test/java/shopping/integration/OrderIntegrationTest.java
@@ -1,22 +1,37 @@
 package shopping.integration;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
-import io.restassured.path.json.config.JsonPathConfig;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import shopping.domain.ExchangeCode;
+import shopping.dto.response.ExchangeResponse;
 import shopping.dto.response.OrderResponse;
+import shopping.infrastructure.CustomRestTemplate;
 
 @DisplayName("주문 기능")
 class OrderIntegrationTest extends IntegrationTest {
 
+    @MockBean
+    CustomRestTemplate mockCustomRestTemplate;
+
     @Test
     @DisplayName("장바구니 아이템 주문에 성공한다")
     void order() {
+        // given
+        when(mockCustomRestTemplate.getResult(anyString(), any()))
+            .thenReturn(createExchangeResponse());
+
         // when
         ExtractableResponse<Response> result = OrderIntegrationSupporter.order();
 
@@ -28,6 +43,10 @@ class OrderIntegrationTest extends IntegrationTest {
     @Test
     @DisplayName("장바구니 아이템 주문에 성공하면 기존 장바구니 아이템들은 모두 제거된다")
     void allCartProductsShouldBeDeleted_WhenOrderSuccess() {
+        // given
+        when(mockCustomRestTemplate.getResult(anyString(), any()))
+            .thenReturn(createExchangeResponse());
+
         // when
         OrderIntegrationSupporter.order();
 
@@ -39,6 +58,10 @@ class OrderIntegrationTest extends IntegrationTest {
     @Test
     @DisplayName("장바구니에 아이템이 없으면 Bad Request 를 반환한다")
     void returnBadRequest_WhenCartProductIsEmpty() {
+        // given
+        when(mockCustomRestTemplate.getResult(anyString(), any()))
+            .thenReturn(createExchangeResponse());
+
         // when
         ExtractableResponse<Response> result = OrderIntegrationSupporter.order("woowa2@woowa.com",
             "1234");
@@ -51,6 +74,9 @@ class OrderIntegrationTest extends IntegrationTest {
     @DisplayName("주문 상세 정보를 조회한다")
     void findMemberOrderById() {
         // given
+        when(mockCustomRestTemplate.getResult(anyString(), any()))
+            .thenReturn(createExchangeResponse());
+
         ExtractableResponse<Response> order = OrderIntegrationSupporter.order();
         String orderId = order.header("Location").split("/")[2];
 
@@ -95,6 +121,9 @@ class OrderIntegrationTest extends IntegrationTest {
     @DisplayName("사용자가 주문 목록을 반환한다")
     void findMemberOrders() {
         // given
+        when(mockCustomRestTemplate.getResult(anyString(), any()))
+            .thenReturn(createExchangeResponse());
+
         OrderIntegrationSupporter.order();
 
         // when
@@ -103,5 +132,11 @@ class OrderIntegrationTest extends IntegrationTest {
         // then
         assertThat(result.statusCode()).isEqualTo(HttpStatus.OK.value());
         assertThat(result.body().as(ArrayList.class)).hasSize(1);
+    }
+
+    private ExchangeResponse createExchangeResponse() {
+        Map<String, Double> map = new HashMap<>();
+        map.put(ExchangeCode.USDKRW.name(), 1302.2);
+        return new ExchangeResponse(map);
     }
 }
