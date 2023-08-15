9ed016147972a91a81be862c9ec8fac638b48a5c
rimrim990 <lim990718@naver.com>
2023-08-15 17:57:12 +0900

    test: 캐시가 만료 상황에 대한 단위 테스트 추가

--- src/test/java/shopping/infrastructure/CacheProviderTest.java
+++ src/test/java/shopping/infrastructure/CacheProviderTest.java
@@ -2,12 +2,15 @@ package shopping.infrastructure;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -18,6 +21,17 @@ import shopping.domain.ExchangeRates;
 @DisplayName("CacheProvider 클래스")
 class CacheProviderTest {
 
+    private CacheProvider<ExchangeRates> cacheProvider;
+    private Clock clock;
+
+    @BeforeEach
+    void setUp() {
+        clock = Mockito.mock(Clock.class);
+        doReturn(ZoneId.systemDefault()).when(clock).getZone();
+        doReturn(Instant.now()).when(clock).instant();
+        cacheProvider = Mockito.spy(new CacheProvider<>(EmptyExchangeRates::new, clock));
+    }
+
     @DisplayName("getCache 메서드는")
     @Nested
     class getCache_Method {
@@ -26,9 +40,6 @@ class CacheProviderTest {
         @DisplayName("데이터가 null 이면 캐싱 데이터를 초기화한다")
         void getCache() {
             // given
-            final Clock fixedClock = createFixedClock(Instant.now());
-            final CacheProvider<ExchangeRates> cacheProvider = createMockCachedProvider(fixedClock);
-
             doCallRealMethod().when(cacheProvider).getData();
 
             // when
@@ -43,24 +54,27 @@ class CacheProviderTest {
         @DisplayName("데이터가 이미 존재하면 캐싱된 데이터를 가져온다")
         void getCache_retrieveCachedData() {
             // given
-            final Clock fixedClock = createFixedClock(Instant.now());
-            final CacheProvider<ExchangeRates> cacheProvider = createMockCachedProvider(fixedClock);
-
             doCallRealMethod().when(cacheProvider).getData();
-
             final ExchangeRates initialData = cacheProvider.getData();
 
             // when & then
             assertThat(cacheProvider.getData()).isEqualTo(initialData);
-            verify(cacheProvider, times(1)).updateCache();
+            verify(cacheProvider).updateCache();
         }
-    }
 
-    private CacheProvider<ExchangeRates> createMockCachedProvider(Clock clock) {
-        return Mockito.spy(new CacheProvider<>(EmptyExchangeRates::new, clock));
-    }
+        @Test
+        @DisplayName("만료시간이 지나면 캐시 데이터를 초기화한다")
+        void getCache_updateExpiredCache() {
+            // given
+            doCallRealMethod().when(cacheProvider).getData();
+            final ExchangeRates initialData = cacheProvider.getData();
 
-    private Clock createFixedClock(Instant timeInstant) {
-        return Clock.fixed(timeInstant, ZoneId.systemDefault());
+            final Instant willExpireAt = Instant.now().plus(2, ChronoUnit.HOURS);
+            doReturn(willExpireAt).when(clock).instant();
+
+            // when & then
+            assertThat(cacheProvider.getData()).isNotEqualTo(initialData);
+            verify(cacheProvider, times(2)).updateCache();
+        }
     }
 }
