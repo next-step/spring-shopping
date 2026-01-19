a707c8227fda5b246dbd2256119321dca7ec59e1
ryeol.jang <ryeol.jang@woowahan.com>
2026-01-19 18:31:40 +0900

    fix: 코드리뷰 반영

--- src/main/kotlin/shopping/controller/model/ProductRequest.kt
+++ src/main/kotlin/shopping/controller/model/ProductRequest.kt
@@ -8,19 +8,20 @@ import jakarta.validation.constraints.Size
 import shopping.repository.model.Product
 
 data class ProductRequest(
+    @field:NotBlank
     @field:Size(min = 1, max = 15)
     @field:Pattern(regexp = "^[a-zA-Z0-9()\\[\\]+\\-&/_ ]+$", message = "Name contains invalid characters.")
-    val name: String,
+    val name: String?,
     @field:Positive
     @field:NotNull
     val price: Int?,
     @field:NotBlank
-    val imageUrl: String,
+    val imageUrl: String?,
 ) {
     fun toEntity(): Product =
         Product(
-            name = this.name,
+            name = this.name!!,
             price = this.price!!,
-            imageUrl = this.imageUrl,
+            imageUrl = this.imageUrl!!,
         )
 }
