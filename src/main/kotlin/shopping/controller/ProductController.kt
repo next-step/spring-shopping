9abe21e472bb4b3b18fa4911cdea9f16ec72e9de
hyungyu.lee <hyungyu.lee@woowahan.com>
2026-01-20 17:47:10 +0900

    refactor: 코드리뷰 반영 - 컨트롤러 응답 형식 통일 - 생성 시 응답코드 201로 나가도록 변경

--- src/main/kotlin/shopping/controller/ProductController.kt
+++ src/main/kotlin/shopping/controller/ProductController.kt
@@ -1,15 +1,8 @@
 package shopping.controller
 
 import jakarta.validation.Valid
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 import shopping.controller.model.ProductRequestBody
 import shopping.controller.model.ProductResponseBody
 import shopping.exception.NotFoundException
@@ -31,12 +24,13 @@ class ProductController(
     @GetMapping("/{id}")
     fun getProduct(
         @PathVariable id: Long,
-    ): ResponseEntity<ProductResponseBody> {
+    ): ProductResponseBody {
         val found = productService.findById(id) ?: throw NotFoundException()
-        return ResponseEntity.ok(ProductResponseBody.from(found))
+        return ProductResponseBody.from(found)
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun createProduct(
         @RequestBody @Valid requestBody: ProductRequestBody,
     ): ProductResponseBody {
@@ -49,18 +43,18 @@ class ProductController(
     fun updateProduct(
         @PathVariable id: Long,
         @RequestBody @Valid requestBody: ProductRequestBody,
-    ): ResponseEntity<ProductResponseBody> {
+    ): ProductResponseBody {
         validate(requestBody)
         val saved = productService.update(id, requestBody.toEntity())
-        return ResponseEntity.ok(ProductResponseBody.from(saved))
+        return ProductResponseBody.from(saved)
     }
 
     @DeleteMapping("/{id}")
     fun deleteProduct(
         @PathVariable id: Long,
-    ): ResponseEntity<String> {
+    ): String {
         productService.deleteById(id)
-        return ResponseEntity.ok("Product with id $id deleted successfully.")
+        return "Product with id $id deleted successfully."
     }
 
     private fun validate(product: ProductRequestBody) {
