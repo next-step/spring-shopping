890a7044edcf237be9bea11e0f021e85a3f709a6
hyungyu.lee <hyungyu.lee@woowahan.com>
2026-01-20 17:42:07 +0900

    refactor: 코드리뷰 반영 - RestTemplate -> RestClient 교체

--- src/main/kotlin/shopping/service/PurgoMalumService.kt
+++ src/main/kotlin/shopping/service/PurgoMalumService.kt
@@ -1,25 +1,21 @@
 package shopping.service
 
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Service
-import org.springframework.web.client.RestTemplate
-import org.springframework.web.client.exchange
+import org.springframework.web.client.RestClient
 
 @Service
 class PurgoMalumService(
-    private val restTemplate: RestTemplate,
+    private val restClient: RestClient
 ) {
     fun containsProfanity(text: String): Boolean {
         if (text.isBlank()) {
             return false
         }
 
-        val url = "http://www.purgomalum.com/service/containsprofanity?text=$text"
-        val headers = HttpHeaders().apply { set("Accept", "text/plain") }
-        val entity = HttpEntity<String>(headers)
-        val result = restTemplate.exchange<String>(url, HttpMethod.GET, entity)
-        return result.body?.toBoolean() ?: throw IllegalStateException("Failed to check profanity.")
+        val response = restClient.get().uri("/service/containsprofanity?text=$text")
+            .header("Accept", "text/plain")
+            .retrieve()
+            .body(String::class.java);
+        return response?.toBoolean() ?: throw IllegalStateException("Failed to check profanity.")
     }
 }
