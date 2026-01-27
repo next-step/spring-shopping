31006e30e060b356dfa0d40105d4ad9b5161d3ec
ssh1224 <ssh1224@woowahan.com>
2026-01-27 23:13:59 +0900

    refact: Service Layer Transactional 정의

--- src/main/kotlin/shopping/application/product/ProductService.kt
+++ src/main/kotlin/shopping/application/product/ProductService.kt
@@ -1,6 +1,7 @@
 package shopping.application.product
 
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import shopping.application.product.validator.ProductValidator
 import shopping.core.product.Product
 import shopping.core.product.ProductRepository
@@ -8,6 +9,7 @@ import shopping.web.product.response.ProductResponse
 import shopping.web.product.response.ProductsResponse
 
 @Service
+@Transactional(readOnly = true)
 class ProductService(
     val productRepository: ProductRepository,
     val productValidator: ProductValidator,
@@ -19,10 +21,12 @@ class ProductService(
         return ProductResponse.fromDomain(product)
     }
 
+    @Transactional
     fun save(product: Product): ProductResponse {
         require(!productValidator.containsProfanity(product.name)) { "Product name is invalid" }
         return ProductResponse.fromDomain(productRepository.save(product))
     }
 
+    @Transactional
     fun deleteById(id: Long) = productRepository.deleteById(id)
 }
