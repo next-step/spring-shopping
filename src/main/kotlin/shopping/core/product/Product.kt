7687416cb7c31711febd7020a72baae6f67feb6a
ssh1224 <ssh1224@woowahan.com>
2026-01-27 23:10:55 +0900

    feat: H2, JPA 반영

--- src/main/kotlin/shopping/core/product/Product.kt
+++ src/main/kotlin/shopping/core/product/Product.kt
@@ -1,11 +1,17 @@
 package shopping.core.product
 
-// 추후 가능하다면 Price 일급객체 선언해서 사용 가능
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Table(name = "products")
+@Entity
 class Product(
-    val name: String,
-    val price: Long,
-    val imageUrl: String,
+    val name: String = "",
+    val price: Long = 0,
+    val imageUrl: String = "",
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0L,
-) {
-    fun copy(id: Long): Product = Product(this.name, this.price, this.imageUrl, id)
-}
+)
