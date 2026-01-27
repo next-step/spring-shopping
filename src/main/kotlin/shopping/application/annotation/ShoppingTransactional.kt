5bcfbe189cdb23232b8f46c06c97a351bbd09531
ssh1224 <ssh1224@woowahan.com>
2026-01-27 23:15:38 +0900

    refact: 커스텀 애노테이션

---
+++ src/main/kotlin/shopping/application/annotation/ShoppingTransactional.kt
@@ -0,0 +1,8 @@
+package shopping.application.annotation
+
+import org.springframework.transaction.annotation.Transactional
+
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+@Transactional
+annotation class ShoppingTransactional
