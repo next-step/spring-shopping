92924ccfad51f902b6b9ec7e2dd9ec4de542c848
yebink <yebink@woowahan.com>
2026-01-30 14:12:14 +0900

    refactor : 코드리뷰 반영 - 커넥션 새로 가져오도록 변경

--- src/main/kotlin/shopping/core/product/ProductRepository.kt
+++ src/main/kotlin/shopping/core/product/ProductRepository.kt
@@ -3,7 +3,6 @@ package shopping.core.product
 import jakarta.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Repository
-import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.Statement
 
@@ -11,8 +10,6 @@ import java.sql.Statement
 class ProductRepository(
     @Value("\${spring.datasource.url}") val url: String,
 ) {
-    private val connection: Connection = DriverManager.getConnection(url)
-
     @PostConstruct
     fun createTable() {
         val sql =
@@ -21,33 +18,39 @@ class ProductRepository(
                 "name varchar(20) not null, " +
                 "price bigint not null, image_url varchar(1000) not null" +
                 ")"
-        connection.createStatement()
-            .use { statement -> statement.execute(sql) }
+        DriverManager.getConnection(url).use { connection ->
+            connection.createStatement().use { statement ->
+                statement.execute(sql)
+            }
+        }
     }
 
     fun findAll(): List<Product> {
         val sql = "select * from product"
-
-        connection.prepareStatement(sql).use { statement ->
-            statement.executeQuery().use { resultSet ->
-                val result = mutableListOf<Product>()
-                while (resultSet.next()) {
-                    result += Product.fromResultSet(resultSet)
+        DriverManager.getConnection(url).use { connection ->
+            connection.prepareStatement(sql).use { statement ->
+                statement.executeQuery().use { resultSet ->
+                    val result = mutableListOf<Product>()
+                    while (resultSet.next()) {
+                        result += Product.fromResultSet(resultSet)
+                    }
+                    return result
                 }
-                return result
             }
         }
     }
 
     fun findById(id: Long): Product {
         val sql = "select * from product where id = ?"
-        connection.prepareStatement(sql).use { statement ->
-            statement.setLong(1, id)
-            statement.executeQuery().use { resultSet ->
-                return if (resultSet.next()) {
-                    Product.fromResultSet(resultSet)
-                } else {
-                    throw IllegalArgumentException("Product not found")
+        DriverManager.getConnection(url).use { connection ->
+            connection.prepareStatement(sql).use { statement ->
+                statement.setLong(1, id)
+                statement.executeQuery().use { resultSet ->
+                    return if (resultSet.next()) {
+                        Product.fromResultSet(resultSet)
+                    } else {
+                        throw IllegalArgumentException("Product not found")
+                    }
                 }
             }
         }
@@ -55,47 +58,55 @@ class ProductRepository(
 
     fun save(product: Product): Product {
         val sql = "insert into product (name, price, image_url) values (?, ?, ?)"
-        connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS).use { statement ->
-            statement.setString(1, product.name)
-            statement.setLong(2, product.price)
-            statement.setString(3, product.imageUrl)
-            statement.executeUpdate()
+        DriverManager.getConnection(url).use { connection ->
+            connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS).use { statement ->
+                statement.setString(1, product.name)
+                statement.setLong(2, product.price)
+                statement.setString(3, product.imageUrl)
+                statement.executeUpdate()
 
-            statement.generatedKeys.use { resultSet ->
-                if (!resultSet.next()) {
-                    throw IllegalStateException("저장에 실패했습니다.")
+                statement.generatedKeys.use { resultSet ->
+                    if (!resultSet.next()) {
+                        throw IllegalStateException("저장에 실패했습니다.")
+                    }
+                    val id = resultSet.getLong(1)
+                    return findById(id)
                 }
-                val id = resultSet.getLong(1)
-                return findById(id)
             }
         }
     }
 
     fun update(product: Product): Product {
         val sql = "update product set name = ?, price = ?, image_url = ? where id = ?"
-        connection.prepareStatement(sql).use { statement ->
-            statement.setString(1, product.name)
-            statement.setLong(2, product.price)
-            statement.setString(3, product.imageUrl)
-            statement.setLong(4, product.id)
-            statement.executeUpdate()
+        DriverManager.getConnection(url).use { connection ->
+            connection.prepareStatement(sql).use { statement ->
+                statement.setString(1, product.name)
+                statement.setLong(2, product.price)
+                statement.setString(3, product.imageUrl)
+                statement.setLong(4, product.id)
+                statement.executeUpdate()
 
-            return findById(product.id)
+                return findById(product.id)
+            }
         }
     }
 
     fun deleteById(id: Long): Boolean {
         val sql = "delete from product where id = ?"
-        connection.prepareStatement(sql).use { statement ->
-            statement.setLong(1, id)
-            return statement.executeUpdate() > 0
+        DriverManager.getConnection(url).use { connection ->
+            connection.prepareStatement(sql).use { statement ->
+                statement.setLong(1, id)
+                return statement.executeUpdate() > 0
+            }
         }
     }
 
     fun deleteAll() {
         val sql = "truncate table product"
-        connection.createStatement().use { statement ->
-            statement.executeUpdate(sql)
+        DriverManager.getConnection(url).use { connection ->
+            connection.createStatement().use { statement ->
+                statement.executeUpdate(sql)
+            }
         }
     }
 }
