d8bb8d768183f2141113cf441405934761700008
hmc7368022 <jinhee7@hyundai.com>
2026-05-08 20:09:16 +0900

    [이진희]    FakeProfanities 구현

--- src/main/kotlin/shopping/profanity/FakeProfanities.kt
+++ src/main/kotlin/shopping/profanity/FakeProfanities.kt
@@ -2,7 +2,11 @@ package shopping.profanity
 
 import org.springframework.stereotype.Service
 
+/**
+ * TODO 외부 호출 구현
+ */
 @Service
-class PurgoMalumProfanities : Profanities {
+class FakeProfanities : Profanities {
+
     override fun contains(text: String): Boolean = false
 }
