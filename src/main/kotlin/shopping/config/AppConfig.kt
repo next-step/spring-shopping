a57d767beedc1903aa09c9941a5dab5c74c5ccbd
harry4455 <xoxolittlemonster4455@gmail.com>
2026-05-08 13:39:11 +0900

    refactor: RestTemplate 빈 생성을 RestTemplateBuilder로 변경

--- src/main/kotlin/shopping/config/AppConfig.kt
+++ src/main/kotlin/shopping/config/AppConfig.kt
@@ -1,5 +1,6 @@
 package shopping.config
 
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
@@ -7,5 +8,5 @@ import org.springframework.web.client.RestTemplate
 @Configuration
 class AppConfig {
     @Bean
-    fun restTemplate(): RestTemplate = RestTemplate()
+    fun restTemplate(builder: RestTemplateBuilder): RestTemplate = builder.build()
 }
