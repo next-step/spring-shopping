a815c9682377a4af158a7aca156cb8b0b275526c
harry4455 <xoxolittlemonster4455@gmail.com>
2026-05-08 15:20:28 +0900

    feat: 상품 Repository 계층 구현 및 DB 저장 테스트 추가

---
+++ src/main/kotlin/shopping/ProductRepository.kt
@@ -0,0 +1,33 @@
+package shopping
+
+import org.springframework.jdbc.core.JdbcTemplate
+import shopping.domain.Product
+import shopping.domain.ProductName
+
+class ProductRepository(private val jdbcTemplate: JdbcTemplate) {
+    fun saveProduct(product: Product): Product {
+        jdbcTemplate.update(
+            "INSERT INTO product (id, name, price, image_url) VALUES (?, ?, ?, ?)",
+            product.id,
+            product.name.name,
+            product.price,
+            product.imageUrl,
+        )
+        return product
+    }
+
+    fun getById(id: Long): Product? {
+        return jdbcTemplate.query(
+            "SELECT id, name, price, image_url FROM product WHERE id = ?",
+            { rs, _ ->
+                Product(
+                    id = rs.getLong("id"),
+                    name = ProductName(rs.getString("name")),
+                    price = rs.getInt("price"),
+                    imageUrl = rs.getString("image_url"),
+                )
+            },
+            id,
+        ).firstOrNull()
+    }
+}
---
+++ src/test/kotlin/shopping/ProductRepositoryTest.kt
@@ -0,0 +1,48 @@
+package shopping
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest
+import org.springframework.jdbc.core.JdbcTemplate
+import shopping.domain.Product
+import shopping.domain.ProductName
+
+@JdbcTest
+class ProductRepositoryTest {
+
+    @Autowired
+    lateinit var jdbcTemplate: JdbcTemplate
+
+    private lateinit var productRepository: ProductRepository
+
+    private val product = Product(
+        id = 1,
+        name = ProductName("아이스 카페 아메리카노 T"),
+        price = 4500,
+        imageUrl = "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg",
+    )
+
+    @BeforeEach
+    fun setUp() {
+        productRepository = ProductRepository(jdbcTemplate)
+    }
+
+    @Test
+    fun `상품을 추가하면 저장한다`() {
+    }
+
+    @Test
+    fun `상품을 저장하면 조회할 수 있다`() {
+        // given
+        productRepository.saveProduct(product)
+
+        // when
+        val result = productRepository.getById(product.id)
+
+        // then
+        result?.name?.name shouldBe "아이스 카페 아메리카노 T"
+        result?.price shouldBe 4500
+    }
+}
